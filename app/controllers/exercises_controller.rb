c5b27df927d52909159f89cd4c4f9985328ec00f
Martin Pärtel <martin.partel@gmail.com>
2011-07-06 17:12:51 +0300

    (minor controller and html cleanups)

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,31 +1,26 @@
 class ExercisesController < ApplicationController
   before_filter :get_course
 
-  # GET /exercises
-  # GET /exercises.json
   def index
     @exercises = @course.exercises
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.json {
         render :json =>
           @exercises.to_json(:only => [:name, :deadline, :publish_date],
                              :methods => [:return_address, :exercise_file])
-
       }
     end
   end
 
-  # GET /exercises/1
   def show
-    @exercise = Exercise.first(:conditions => { :course_id => @course.id,
-                                                :id => params[:id] })
+    @exercise = @course.exercises.find(params[:id])
     @exercise_return = ExerciseReturn.new
     @exercise_returns = ExerciseReturn.where(:exercise_id => @exercise.id).order("(created_at)DESC LIMIT 15")
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.zip {
         send_file @course.exercise_file(@exercise.name)
       }
