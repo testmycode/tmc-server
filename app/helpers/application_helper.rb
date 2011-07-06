56a6b838906c0b0ed9ffed4df800cff5621faa54
Martin Pärtel <martin.partel@gmail.com>
2011-07-06 18:08:36 +0300

    Added helpers ApplicationHelper::labeled(_form).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def labeled(label, tags)
+    if tags =~ /id\s*=\s*"([^"]+)"/
+      raw('<label for="' + h($1) + '">' + h(label) + '</label>' + tags)
+    else
+      raise 'Cannot label a tag without an id'
+    end
+  end
+  
+  def labeled_field(label, tags)
+    raw('<div class="field">' + labeled(label, tags) + '</div>')
+  end
 end
