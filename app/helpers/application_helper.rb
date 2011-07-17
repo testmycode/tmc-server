85e5585b783fae1acd23a35a93c9a6fdc5bd7373
Martin Pärtel <martin.partel@gmail.com>
2011-07-18 01:36:45 +0300

    Used datatables for some tables.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,19 @@ module ApplicationHelper
   def labeled_field(label, tags)
     raw('<div class="field">' + labeled(label, tags) + '</div>')
   end
+  
+  def use_datatables(table_selector)
+    script =<<EOS
+<script type="text/javascript">
+<!--
+$(document).ready(function() {
+  $('#{escape_javascript table_selector}').dataTable({
+    bJQueryUI: true
+  });
+});
+//-->
+</script>
+EOS
+    raw(script)
+  end
 end
