eef790d0932410bd0c5ec0a5c687b69c8c1de248
Martin Pärtel <martin.partel@gmail.com>
2011-09-01 03:22:51 +0300

    usermanual_spec: switch database cleaner strategy back when done.

--- spec/usermanual/usermanual_spec.rb
+++ spec/usermanual/usermanual_spec.rb
@@ -4,8 +4,16 @@ describe "User manual", :type => :request, :usermanual => true do
   
   before :each do
     # switch from transaction strategy to truncation strategy
+    # so that DB changes are visible to the browser
     DatabaseCleaner.clean # end the transaction
     DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+  
+  after :each do
+    # switch back to transaction strategy
+    DatabaseCleaner.clean
+    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.start
   end
   
