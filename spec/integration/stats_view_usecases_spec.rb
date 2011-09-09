eab0c81c95f0481d1351bcd1aab6d873485367ef
Martti Rannanjärvi <mrannanj@cs.helsinki.fi>
2011-09-10 02:25:55 +0300

    Username -> Student number in tests

--- spec/integration/stats_view_usecases_spec.rb
+++ spec/integration/stats_view_usecases_spec.rb
@@ -2,26 +2,26 @@ require 'spec_helper'
 
 describe "The system (used by an instructor for viewing statistics)" do
   include IntegrationTestActions
-  
+
   before :each do
     course = Course.create!(:name => 'mycourse')
     repo = clone_course_repo(course)
     repo.copy_simple_exercise('EasyExercise')
     repo.copy_simple_exercise('HardExercise')
     repo.add_commit_push
-    
+
     course.refresh
   end
-  
+
   it "should show all submissions for an exercise" do
     submit_exercise('EasyExercise', :solve => true, :username => '123')
     submit_exercise('EasyExercise', :solve => false, :username => '456')
     submit_exercise('EasyExercise', :compilation_error => true, :username => '789')
-    
+
     log_in_as_instructor
-    click_link 'mycourse' 
+    click_link 'mycourse'
     click_link 'EasyExercise'
-    
+
     page.should have_content('123')
     page.should have_content('456')
     page.should have_content('789')
@@ -29,32 +29,32 @@ describe "The system (used by an instructor for viewing statistics)" do
     page.should have_content('Fail')
     page.should have_content('Error')
   end
-  
+
   def submit_exercise(exercise_name, options = {})
     options = {
       :solve => true,
       :compilation_error => false,
       :username => 'some_username'
     }.merge(options)
-    
+
     FileUtils.rm_rf exercise_name
     ex = SimpleExercise.new(exercise_name)
     ex.solve_all if options[:solve]
     ex.introduce_compilation_error('oops') if options[:compilation_error]
     ex.make_zip
-    
+
     visit '/'
     click_link 'mycourse'
     click_link exercise_name
-    fill_in 'Username', :with => options[:username]
+    fill_in 'Student number', :with => options[:username]
     attach_file('Zipped project', "#{exercise_name}.zip")
     click_button 'Submit'
   end
-  
+
   def log_in_as_instructor
     visit '/'
     user = User.create!(:login => 'user', :password => 'xooxer', :administrator => true)
     log_in_as(user.login)
   end
-  
+
 end
--- spec/integration/student_usecases_spec.rb
+++ spec/integration/student_usecases_spec.rb
@@ -2,103 +2,103 @@ require 'spec_helper'
 
 describe "The system (used by a student)" do
   include IntegrationTestActions
-  
+
   before :each do
     @course = Course.create!(:name => 'mycourse')
     @repo = clone_course_repo(@course)
     @repo.copy_simple_exercise('MyExercise')
     @repo.add_commit_push
-    
+
     @course.refresh
-    
+
     visit '/'
     click_link 'mycourse'
   end
-  
+
   it "should offer exercises as downloadable zips" do
     click_link('zip')
     File.open('MyExercise.zip', 'wb') {|f| f.write(page.source) }
     system!("unzip -qq MyExercise.zip")
-    
+
     File.should be_a_directory('MyExercise')
     File.should exist('MyExercise/src/SimpleStuff.java')
     File.should exist('MyExercise/test/SimpleTest.java')
     File.should_not exist('MyExercise/test/SimpleHiddenTest.java')
   end
-  
+
   it "should show successful test results for correct solutions" do
     ex = SimpleExercise.new('MyExercise')
     ex.solve_all
     ex.make_zip
-    
+
     click_link 'MyExercise'
-    fill_in 'Username', :with => '123'
+    fill_in 'Student number', :with => '123'
     attach_file('Zipped project', 'MyExercise.zip')
     click_button 'Submit'
-    
+
     page.should have_content('All tests successful')
     page.should have_content('Ok')
     page.should_not have_content('Fail')
   end
-  
+
   it "should show unsuccessful test results for incorrect solutions" do
     ex = SimpleExercise.new('MyExercise')
     ex.make_zip
-    
+
     click_link 'MyExercise'
-    fill_in 'Username', :with => '123'
+    fill_in 'Student number', :with => '123'
     attach_file('Zipped project', 'MyExercise.zip')
     click_button 'Submit'
-    
+
     page.should have_content('Some tests failed')
     page.should have_content('Fail')
   end
-  
+
   it "should show compilation error for uncompilable solutions" do
     ex = SimpleExercise.new('MyExercise')
     ex.introduce_compilation_error('oops')
     ex.make_zip
-    
+
     click_link 'MyExercise'
-    fill_in 'Username', :with => '123'
+    fill_in 'Student number', :with => '123'
     attach_file('Zipped project', 'MyExercise.zip')
     click_button 'Submit'
-    
+
     page.should have_content('Compilation error')
     page.should have_content('oops')
   end
-  
+
   it "should not show exercises that have been explicitly hidden" do
     @repo.set_metadata_in('MyExercise', 'hidden' => true)
     @repo.add_commit_push
     @course.refresh
-    
+
     visit '/'
     click_link 'mycourse'
-    
+
     page.should_not have_content('MyExercise')
   end
-  
+
   it "should show exercises whose deadline has passed but without a submission form" do
     @repo.set_metadata_in('MyExercise', 'deadline' => Date.yesterday.to_s)
     @repo.add_commit_push
     @course.refresh
-    
+
     visit '/'
     click_link 'mycourse'
-    
+
     page.should have_content('MyExercise')
     page.should have_content('(expired)')
-    
+
     click_link 'MyExercise'
     page.should have_content('(expired)')
     page.should_not have_content('Submit answer')
     page.should_not have_content('Zipped project')
   end
-  
+
   it "should not accept submissions for exercises whose deadline has passed"
-  
+
   it "should not accept submissions for hidden courses"
-  
+
   it "should not show the submission form for unreturnable exercises"
 end
