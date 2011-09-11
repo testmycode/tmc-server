afe36749c0104a286c651d4e9e2ac0e3b630940d
Martti Rannanjärvi <mrannanj@cs.helsinki.fi>
2011-09-12 01:55:16 +0300

    session store from cookie to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110910002217) do
+ActiveRecord::Schema.define(:version => 20110911224755) do
 
   create_table "available_points", :force => true do |t|
     t.integer "exercise_id", :null => false
@@ -54,6 +54,16 @@ ActiveRecord::Schema.define(:version => 20110910002217) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "submissions", :force => true do |t|
     t.integer  "user_id"
