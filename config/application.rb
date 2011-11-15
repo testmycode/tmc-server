a59dd7cd95561fafab1aff92c67c28ab33b4b2ee
Martin Pärtel <martin.partel@gmail.com>
2011-11-15 13:44:24 +0200

    filter submission_file from logs

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module SandboxServer
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :submission_file]
 
     # Enable the asset pipeline [TODO: later]
     config.assets.enabled = false
