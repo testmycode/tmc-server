4b1c713baaac822346d0a245ed261b1d4e025a32
Martin Pärtel <martin.partel@gmail.com>
2012-01-10 18:08:34 +0200

    Updated user manual.

--- spec/usermanual/Factorial.java
+++ spec/usermanual/Factorial.java
@@ -1,7 +1,14 @@
 
 public class Factorial {
     public static long factorial(long n) {
-        return 0; // Please write your code here
+        // BEGIN SOLUTION
+        long result = 1;
+        for (long i = 2; i <= n; ++i) {
+            result *= i;
+        }
+        return result;
+        // END SOLUTION
+        // STUB: return 0; // Please write your code here
     }
 }
 
