ad61a1d8ecd5d89ff43695ab6bd23d8b29a7de69
Martin Pärtel <martin.partel@gmail.com>
2012-01-13 13:02:09 +0200

    Show stack traces on submission page.

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -3,4 +3,13 @@ module SubmissionsHelper
     status = submission.status
     raw("<span class=\"#{status}\">#{status.to_s.capitalize}</span>")
   end
+  
+  def format_stack_trace(stack_trace)
+    result = ActiveSupport::SafeBuffer.new('')
+    stack_trace.each do |line|
+      result << "#{line['fileName']}:#{line['lineNumber']}: #{line['declaringClass']}.#{line['methodName']}"
+      result << tag(:br)
+    end
+    result
+  end
 end
