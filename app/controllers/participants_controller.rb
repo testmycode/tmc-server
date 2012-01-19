6b0631316a26c863ffc643be62f98e791ef531e9
Martin Pärtel <martin.partel@gmail.com>
2012-01-19 05:27:30 +0200

    Added /participants.json

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -4,6 +4,19 @@ class ParticipantsController < ApplicationController
   def index
     return respond_access_denied unless current_user.administrator?
     @participants = User.where(:administrator => false).order(:login)
+    respond_to do |format|
+      format.html
+      format.json do
+        result = []
+        @participants.each do |user|
+          result << { :id => user.id, :username => user.login, :email => user.email }
+        end
+        render :json => {
+          :api_version => API_VERSION,
+          :participants => result
+        }
+      end
+    end
   end
 
 end
