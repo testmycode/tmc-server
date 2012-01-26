013af0947ed9458c33741126c2a258f242278fd2
Martin Pärtel <martin.partel@gmail.com>
2012-01-26 05:05:04 +0200

    Significantly optimized points summary view.

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -4,9 +4,12 @@ class PointsController < ApplicationController
 
   def index
     @course = Course.find(params[:course_id])
-    users = User.course_students(@course).sort!
     sheets = @course.gdocs_sheets.sort!
-    @summary = summary_hash(@course, users, sheets)
+    @summary = summary_hash(@course, sheets)
+    
+    if params[:sort_by] == 'total_points'
+      @summary.users.sort_by {|username| @summary[:total_for_user][username].to_i }
+    end
   end
 
   def refresh_gdocs
@@ -25,23 +28,34 @@ class PointsController < ApplicationController
       course_gdocs_sheet_exercises(@course, @sheetname).sort!
   end
 
-  def summary_hash(course, users, sheets)
+  def summary_hash(course, sheets)
+    per_user_and_sheet = {}
+    for sheet in sheets
+      for record in AwardedPoint.count_per_user_in_course_with_sheet(course, sheet)
+        username = record['login']
+        count = record['count'].to_i
+        per_user_and_sheet[username] ||= {}
+        per_user_and_sheet[username][sheet] = count
+      end
+    end
+    
+    user_totals = {}
+    for username, per_sheet in per_user_and_sheet
+      user_totals[username] ||= 0
+      user_totals[username] += per_sheet.values.reduce(0, &:+)
+    end
+    
     {
       :sheets => sheets.map{|sheet| {
         :name => sheet,
-        :available => AvailablePoint.course_sheet_points(course, sheet).length,
-        :awarded => AwardedPoint.course_sheet_points(course, sheet).length
+        :total_available => AvailablePoint.course_sheet_points(course, sheet).length,
+        :total_awarded => AwardedPoint.course_sheet_points(course, sheet).length
       }},
-      :awarded => AwardedPoint.course_points(course).length,
-      :available => AvailablePoint.course_points(course).length,
-      :students => users.map{|u| {
-        :login => u.login,
-        :points => sheets.reduce({}){ |hash, sheet|
-          hash.merge({ sheet => AwardedPoint.
-            course_user_sheet_points(course, u, sheet).length })
-          },
-        :awarded => AwardedPoint.course_user_points(course, u).length
-      }}
+      :total_awarded => AwardedPoint.course_points(course).length,
+      :total_available => AvailablePoint.course_points(course).length,
+      :awarded_for_user_and_sheet => per_user_and_sheet,
+      :total_for_user => user_totals,
+      :users => per_user_and_sheet.keys.sort
     }
   end
 end
--- app/models/awarded_point.rb
+++ app/models/awarded_point.rb
@@ -36,5 +36,28 @@ class AwardedPoint < ActiveRecord::Base
     where(:exercises => { :gdocs_sheet => sheetname }).
     group("awarded_points.id")
   }
+  
+  # Gets a hash of user to count of points awarded for exercises of the given sheet
+  def self.count_per_user_in_course_with_sheet(course, sheetname)
+    users = User.arel_table
+    awarded_points = AwardedPoint.arel_table
+    submissions = Submission.arel_table
+    
+    exercise_names = course.exercises.where(:gdocs_sheet => sheetname).map(&:name)
+    
+    sql =
+      awarded_points.
+      project([users[:login].as('login'), Arel.sql('COUNT(*)').as('count')]).
+      join(users).on(awarded_points[:user_id].eq(users[:id])).
+      join(submissions).on(awarded_points[:submission_id].eq(submissions[:id])).
+      where(awarded_points[:course_id].eq(course.id)).
+      where(awarded_points[:user_id].eq(users[:id])).
+      where(submissions[:exercise_name].in(exercise_names)).
+      group(users[:login]).
+      order(users[:login]).
+      to_sql
+    
+    ActiveRecord::Base.connection.execute(sql).to_a
+  end
 
 end
