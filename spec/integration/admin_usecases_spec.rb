b1a96a53f02e3d62e24d37917405f760fd4b1a4f
Martin Pärtel <martin.partel@gmail.com>
2012-02-15 07:57:29 +0200

    Fixed a submission rerun test.

--- spec/integration/admin_usecases_spec.rb
+++ spec/integration/admin_usecases_spec.rb
@@ -47,6 +47,7 @@ describe "The system (used by an instructor for administration)", :integration =
     
     click_button 'Rerun submission'
     page.should have_content('Rerun scheduled')
+    SubmissionProcessor.new.reprocess_timed_out_submissions
     wait_for_submission_to_be_processed
     
     page.should_not have_content('some funny error')
