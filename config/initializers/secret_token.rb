ade4f8a82fdad70374d63d289382105d6cad7290
Martin Pärtel <martin.partel@gmail.com>
2012-04-03 01:28:28 +0300

    Remove unused config/initializers/secret_token.rb.
    Not used by database session store and shouldn't be public anyway.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TmcServer::Application.config.secret_token = 'ca4c4db06798dbe05d33d757700f26c2e2d6ac15d275ab841430761e30b5b6363481fa731f06725ff3bbfcaa07a33b0426466ea420d3eb81b953cfbaf16c53fc'
