256fa6d9c8f7bd68cd08e5e1cb9d89d9fe942573
Martin Pärtel <martin.partel@gmail.com>
2012-04-03 01:43:22 +0300

    Made session cookie key configurable in site.yml.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,5 +5,5 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-TmcServer::Application.config.session_store :active_record_store
+TmcServer::Application.config.session_store :active_record_store, :key => SiteSetting.value(:session_cookie_key)
 
