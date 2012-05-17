2f7fad84392f6773c9a9157692850e41cdf969c4
Martin Pärtel <martin.partel@gmail.com>
2012-05-17 19:04:36 +0300

    PointComparison to use natcmp.

--- app/models/point_comparison.rb
+++ app/models/point_comparison.rb
@@ -1,24 +1,7 @@
 module PointComparison
   extend Comparable
-
-  def name=(new_name)
-    super
-    @name_is_numeric = nil
-  end
-
-  def name_is_numeric
-    @name_is_numeric ||= !!(name =~ /^\d+(?:\.\d?)?$/)
-  end
-
+  
   def <=>(other)
-    if self.name_is_numeric && other.name_is_numeric
-      self.name.to_f <=> other.name.to_f
-    elsif self.name_is_numeric
-      -1 # numeric ones first
-    elsif other.name_is_numeric
-      1
-    else
-      self.name <=> other.name
-    end
+    Natcmp.natcmp(self.name, other.name)
   end
 end
