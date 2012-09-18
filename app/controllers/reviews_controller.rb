6e4e8415201daff1118acf8b60276ef125b6422f
Martin Pärtel <martin.partel@gmail.com>
2012-09-18 16:16:37 +0300

    Worked on code review UI.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,21 +6,44 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    fetch_submission_and_files
-    @new_review = @submission.reviews.new
+    fetch_submission
+    fetch_files
+    @new_review = Review.new(
+      :submission => @submission,
+      :reviewer => current_user
+    )
     authorize! :create, @review
     @show_page_presence = true
     render 'reviews/show'
   end
 
   def show
-    fetch_submission_and_files
+    fetch_submission
+    fetch_files
+  end
+
+  def create
+    @review = Review.new(
+      :submission_id => params[:submission_id],
+      :reviewer_id => current_user.id,
+      :review_body => params[:review][:review_body]
+    )
+    authorize! :create, @review
+    if @review.save
+      flash[:success] = 'Code review added.'
+      redirect_to new_submission_review_path(@review.submission_id)
+    else
+      respond_with_error('Failed to save code review.')
+    end
   end
 
 private
-  def fetch_submission_and_files
+  def fetch_submission
     @submission = Submission.find(params[:submission_id])
-    @files = SourceFileList.for_submission(@submission)
     authorize! :read, @submission
+  end
+
+  def fetch_files
+    @files = SourceFileList.for_submission(@submission)
   end
 end
