c44a92ca03433c96d67e420efddab9245c5f4cd9
Martin Pärtel <martin.partel@gmail.com>
2012-10-11 13:59:16 +0300

    site.yml: removed subcategory under 'emails'.

--- app/mailers/password_reset_key_mailer.rb
+++ app/mailers/password_reset_key_mailer.rb
@@ -1,8 +1,8 @@
 class PasswordResetKeyMailer < ActionMailer::Base
   def reset_link_email(user, key)
-    settings = SiteSetting.value('emails')['reset_password']
+    settings = SiteSetting.value('emails')
     
-    subject = 'TMC Password Reset'
+    subject = '[TMC] Password Reset'
     @url = settings['baseurl'].sub(/\/+$/, '') + '/reset_password/' + key.code
     mail(:from => settings['from'], :to => user.email, :subject => subject)
   end
