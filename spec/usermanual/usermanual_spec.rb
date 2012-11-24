85db29fbebe2c76478f70a3c95161aeda401b987
Martin Pärtel <martin.partel@gmail.com>
2012-11-24 16:34:44 +0200

    Documented `unlocked_for` and `deadline`.

--- spec/usermanual/usermanual_spec.rb
+++ spec/usermanual/usermanual_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe "User manual", :type => :request, :usermanual => true, :integration => true do
+  def make_page(name)
+    doc = DocGen.new(name, self)
+    doc.render_template(File.join(File.dirname(__FILE__), "#{name}.html.erb"))
+  end
+
   it "has a page for instructors" do
-    doc = DocGen.new("instructors", self)
-    doc.render_template(File.join(File.dirname(__FILE__), "instructors.html.erb"))
+    make_page 'instructors'
   end
 end
 
