a0a310fe3a3a8eb2e82a786c3a612c04118c9d1a
Matti Luukkainen <mluukkai@iki.com>
2013-01-27 17:09:24 +0200

    minor refactorings of ExerciseStatusGenerator

--- lib/exercise_status_generator.rb
+++ lib/exercise_status_generator.rb
@@ -1,19 +1,4 @@
 class ExerciseStatusGenerator
-  def self.tmp
-    Exercise.find_all_by_course_id(1, :include => :available_points)
-  end
-
-  def self.tmp2 exercise_name
-    Exercise.find_by_course_id_and_name(1, exercise_name)
-  end
-
-  def self.tmp3
-    points_of_exercise = {}
-    Exercise.find_all_by_course_id(1, :include => :available_points).each { |exercise|
-      points_of_exercise[exercise.id] = exercise.available_points.map(&:name)
-    }
-    points_of_exercise
-  end
 
   def self.completion_status_with awarded_points, submissions, course_id
     points_of_exercise = {}
@@ -23,7 +8,7 @@ class ExerciseStatusGenerator
 
     completion_status = {}
     points_of_exercise.keys.each { |exercise|
-      completion_status[exercise] = completion_status_of_exercise_old points_of_exercise[exercise], awarded_points
+      completion_status[exercise] = completion_status_of_exercise points_of_exercise[exercise], awarded_points
     }
 
     # take into account submissions with zero passing tests
@@ -39,32 +24,17 @@ class ExerciseStatusGenerator
 
   private
 
-  def self.completion_status_of_exercise_old required_points, awarded_points
-     points_not_awarded = required_points - awarded_points
+  def self.completion_status_of_exercise required_points, awarded_points
 
-     if points_not_awarded.empty?
-       return "exercise p100"
-     elsif required_points != points_not_awarded
-       return "exercise p#{percentage(points_not_awarded, required_points)}"
-     else
-       return ""
-     end
-
+     return "" if required_points == (required_points - awarded_points)
+     return "exercise p#{percentage_of_completed_exercises( required_points - awarded_points, required_points)}"
    end
 
-   def self.percentage set1, set2
-     perc = ((10*(set2.size-set1.size))/set2.size)*10
+   def self.percentage_of_completed_exercises not_awarded, required
+     return 100 if not_awarded.empty?
+
+     perc = ((10*(required.size-not_awarded.size))/required.size)*10
      [[10, perc].max, 90].min
    end
 
-  def self.completion_status_of_exercise required_points, awarded_points
-    return "" if awarded_points.empty?
-    return "exercise p#{percentage_of_completed_exercises(awarded_points, required_points)}"
-  end
-
-  def self.percentage_of_completed_exercises awarded, total
-    return 100 if awarded == total
-    points_total = ((10*(awarded.size))/total.size)*10
-    [[10, points_total].max, 90].min
-  end
 end
