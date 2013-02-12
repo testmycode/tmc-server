9ddd8216782a37b1cb7c0860e15e4e2d8081c57a
Martin Pärtel <martin.partel@gmail.com>
2013-02-12 03:12:21 +0200

    Fix describer for UnlockSpecs like "[n] points in [ex]".
    Fixes #116.

--- app/models/unlock_spec.rb
+++ app/models/unlock_spec.rb
@@ -122,7 +122,7 @@ private
       @universal_descriptions << "#{num_points} #{plural(num_points, 'point')} from #{group}"
       @describers << lambda do |u|
         awarded = available_and_awarded(course, group, u)[1]
-        remaining = num_points - awarded
+        remaining = num_points - awarded.count
         if remaining > 0
           "get #{remaining} more #{plural(remaining, 'point')} from #{group}"
         else
