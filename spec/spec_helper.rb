ddc6bb4579a77c071dcdf16dd2c264c578be0573
Jarmo Isotalo <jamo@isotalo.fi>
2013-02-18 10:00:14 +0200

    test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'etc'
 require 'fileutils'
+require 'simplecov'
+SimpleCov.start 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
