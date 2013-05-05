389cf0c7446f9a450f7e63db6b7d84c3a5827a9e
Martin Pärtel <martin.partel@gmail.com>
2013-05-05 16:16:34 +0300

    Better errors if the student does System.exit().

---
+++ spec/fixtures/exercises/malicious/Exit0/src/Main.java
@@ -0,0 +1,7 @@
+import java.io.File;
+
+public class Main {
+    public static void theMethod() throws Exception {
+        System.exit(0);
+    }
+}
--- spec/integration/running_tests/malicious_code_spec.rb
+++ spec/integration/running_tests/malicious_code_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe RemoteSandboxForTesting, :integration => true do
+  def setup_and_run(exercise_name)
+    setup = SubmissionTestSetup.new(:exercise_name => exercise_name)
+    submission = setup.submission
+
+    setup.make_zip
+    RemoteSandboxForTesting.run_submission(submission)
+
+    setup
+  end
+
   describe "when the student's code attempts to delete tests" do
     it "should not leave tests unrun" do
-      setup = SubmissionTestSetup.new(:exercise_name => 'malicious/TestDeleter')
+      setup = setup_and_run('malicious/TestDeleter')
       submission = setup.submission
-      
-      setup.make_zip
-      RemoteSandboxForTesting.run_submission(submission)
       
       submission.test_case_runs.size.should == 2
       submission.test_case_runs.should_not be_all(&:successful)
@@ -17,6 +24,24 @@ describe RemoteSandboxForTesting, :integration => true do
       case_names.sort.should == ['ATest test1', 'BTest test2']
       case_messages = submission.test_case_runs.map(&:message)
       case_messages.should include("Failed to run test.")
+    end
+  end
+
+  describe "when the student's code does System.exit(0)" do
+    it "should report this as a likely cause for missing test results" do
+      setup = setup_and_run('malicious/Exit0')
+      submission = setup.submission
+
+      submission.pretest_error.should == 'Invalid or missing test output. Did you terminate your program with an exit() command?'
+    end
+  end
+
+  describe "when the student's code does System.exit(1)" do
+    it "should report this as a likely cause for missing test results" do
+      setup = setup_and_run('malicious/Exit1')
+      submission = setup.submission
+
+      submission.pretest_error.should include(' (did you use an exit() command?)')
     end
   end
 end
