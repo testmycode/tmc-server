2bddc7b1eb0b3818efb459b9961fda99c7524dec
Jarmo Isotalo <jamo@isotalo.fi>
2013-09-21 00:08:11 +0300

    Fix white space and added method for invalid byte sequence

--- spec/support/fixture_exercise/simple_exercise.rb
+++ spec/support/fixture_exercise/simple_exercise.rb
@@ -10,72 +10,75 @@ class FixtureExercise::SimpleExercise < FixtureExercise
   def initialize(path = 'SimpleExercise', options = {})
     options = {
       :fixture_name => 'SimpleExercise'
-    }.merge(options)
-    super(options[:fixture_name], path)
-  end
-
-  def solve_all
-    solve_addsub
-    solve_mul
-  end
-
-  def solve_addsub
-    solve_add
-    solve_sub
-  end
-  
-  def solve_add
-    replace_method_body_in_file(simple_stuff_path, 'add', 'return a + b;')
-  end
-  
-  def solve_sub
-    replace_method_body_in_file(simple_stuff_path, 'sub', 'return a - b;')
-  end
-  
-  def solve_mul
-    replace_method_body_in_file(simple_stuff_path, 'mul', 'return a * b;')
-  end
-  
-  def write_empty_method_body(code)
-    replace_method_body_in_file(simple_stuff_path, 'emptyMethod', code)
-  end
-  
-  def introduce_compilation_error(text = 'the compiler should fail here')
-    replace_method_body_in_file(simple_stuff_path, 'add', "BAD INPUT #{text}")
-  end
-  
-  def simple_stuff_path
-    "#{java_src_path}/SimpleStuff.java"
-  end
-  
-  def simple_test_path
-    "#{java_test_path}/SimpleTest.java"
-  end
-  
-  def simple_hidden_test_path
-    "#{java_test_path}/SimpleHiddenTest.java"
-  end
-
-  def java_src_path
-    "#{@path}/src"
-  end
-
-  def java_test_path
-    "#{@path}/test"
-  end
-  
-private
-
-  def replace_method_body_in_file(path, method, body)
-    lines = IO.readlines(path)
-    lines = lines.map do |line|
-      if line.include? "METHOD BODY #{method}"
-        body + " // METHOD BODY #{method}\n"
-      else
-        line
-      end
+      }.merge(options)
+      super(options[:fixture_name], path)
     end
-    File.open(path, "wb") {|f| f.write(lines.join) }
+
+    def solve_all
+      solve_addsub
+      solve_mul
+    end
+
+    def solve_addsub
+      solve_add
+      solve_sub
+    end
+
+    def solve_add
+      replace_method_body_in_file(simple_stuff_path, 'add', 'return a + b;')
+    end
+
+    def solve_sub
+      replace_method_body_in_file(simple_stuff_path, 'sub', 'return a - b;')
+    end
+
+    def solve_mul
+      replace_method_body_in_file(simple_stuff_path, 'mul', 'return a * b;')
+    end
+
+    def write_empty_method_body(code)
+      replace_method_body_in_file(simple_stuff_path, 'emptyMethod', code)
+    end
+
+    def introduce_compilation_error(text = 'the compiler should fail here')
+      replace_method_body_in_file(simple_stuff_path, 'add', "BAD INPUT #{text}")
+    end
+
+    def introduce_invalid_byte_sequence
+      replace_method_body_in_file(simple_stuff_path, 'add', "return 0; // bad: \xc3\x28 okay")
+    end
+
+    def simple_stuff_path
+      "#{java_src_path}/SimpleStuff.java"
+    end
+
+    def simple_test_path
+      "#{java_test_path}/SimpleTest.java"
+    end
+
+    def simple_hidden_test_path
+      "#{java_test_path}/SimpleHiddenTest.java"
+    end
+
+    def java_src_path
+      "#{@path}/src"
+    end
+
+    def java_test_path
+      "#{@path}/test"
+    end
+
+    private
+
+    def replace_method_body_in_file(path, method, body)
+      lines = IO.readlines(path)
+      lines = lines.map do |line|
+        if line.include? "METHOD BODY #{method}"
+          body + " // METHOD BODY #{method}\n"
+        else
+          line
+        end
+      File.open(path, "wb") {|f| f.write(lines.join) }
+    end
   end
 end
-
