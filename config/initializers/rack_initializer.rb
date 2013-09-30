ae26486c5a0da7ae31fc023b6535a052b777625f
Martin Pärtel <martin.partel@gmail.com>
2013-09-30 21:51:13 +0300

    MOAR KEY_SPACE

--- config/initializers/rack_initializer.rb
+++ config/initializers/rack_initializer.rb
@@ -3,7 +3,10 @@
 # http://stackoverflow.com/questions/12243694/getting-error-exceeded-available-parameter-key-space
 # or
 # http://stackoverflow.com/questions/9122411/rails-javascript-too-many-parameter-keys-whats-a-good-way-to-normalize-f
+#
+# This file should be removed once everyone is using NB plugin 0.4.1+, which sends events in smaller batches.
+#
 
 if Rack::Utils.respond_to?("key_space_limit=")
-  Rack::Utils.key_space_limit = 262144
+  Rack::Utils.key_space_limit = 262144 * 100
 end
