82b3370ca469a3a91bb21b6242c4727870142609
Jarmo Isotalo <jamo@isotalo.fi>
2013-12-15 23:23:54 +0200

    Try raw sql and mass insert

--- app/controllers/student_events_controller.rb
+++ app/controllers/student_events_controller.rb
@@ -7,6 +7,8 @@ class StudentEventsController < ApplicationController
 
     event_records = params['events'].values
 
+    inserts = []
+    #events = []
     File.open(params['data'].tempfile.path, 'rb') do |data_file|
       ActiveRecord::Base.connection.transaction(:requires_new => true) do
         event_records.each do |record|
@@ -20,8 +22,13 @@ class StudentEventsController < ApplicationController
           data = data_file.read(record['data_length'].to_i)
 
           check_json_syntax(metadata) if metadata
+          metadata = "" if metadata.blank?
+          data = "" if data.blank?
+          # inserts << "(user.id, record['course_name'], record['exercise_name'], event_type, metadata, data, happened_at, system_nano_time)"
 
-          event = StudentEvent.new(
+          #event = StudentEvent.new(
+
+          event = {
             :user_id => user.id,
             :course_name => record['course_name'],
             :exercise_name => record['exercise_name'],
@@ -30,9 +37,22 @@ class StudentEventsController < ApplicationController
             :data => data,
             :happened_at => happened_at,
             :system_nano_time => system_nano_time
-          )
-          event.save!
+          }
+          inserts << "('#{event.values.join("','")}')"#.gsub('"', "'")
+          #)
+          #events << event
+          #if events.size > 30
+          #  StudentEvent.import events.shift(30)
+          #end
+          #event.save!
         end
+        #StudentEvent.import events
+        #
+        sql = "INSERT INTO student_events (user_id, course_name, exercise_name, event_type, metadata_json, data, happened_at, system_nano_time) VALUES #{inserts.join(', ')}"
+#        puts "#{'*'* 200}"
+#        puts sql
+#        puts "#{'*'* 200}"
+        StudentEvent.connection.execute sql
       end
     end
 
@@ -43,7 +63,7 @@ class StudentEventsController < ApplicationController
     end
   end
 
-private
+  private
 
   def check_json_syntax(string)
     ActiveSupport::JSON.decode(string)
