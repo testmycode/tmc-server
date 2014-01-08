5741b1c0727b353c4d3d56524ba6344498188d12
Jarmo Isotalo <jamo@isotalo.fi>
2014-01-08 15:54:37 +0200

    added required logic for showing paste and no paste files

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -2,16 +2,18 @@
 class FilesController < ApplicationController
   skip_authorization_check
   before_filter :check_access
-
+  before_filter :find_submission
   def index
-    @submission = Submission.find(params[:submission_id])
-
     @exercise = @submission.exercise
     @course = @exercise.course
     add_course_breadcrumb
     add_exercise_breadcrumb
     add_submission_breadcrumb
-    add_breadcrumb 'Files', submission_files_path(@submission)
+    if params[:paste_key]
+      add_breadcrumb 'Paste', paste_path(@submission.paste_key)
+    else
+      add_breadcrumb 'Files', submission_files_path(@submission)
+    end
 
     @title = "Submission ##{@submission.id} files"
     @files = SourceFileList.for_submission(@submission)
@@ -19,9 +21,15 @@ class FilesController < ApplicationController
 
   private
   def check_access
-    submission = Submission.find(params[:submission_id])
     respond_access_denied unless current_user.administrator? or submission.user_id.to_s == current_user.id.to_s or (submission.public? and submission.exercise.completed_by?(current_user))
-      
+  end
+
+  def find_submission
+    @submission = if params[:paste_key]
+      Submission.find_by_paste_key!(params[:paste_key])
+    else
+      Submission.find_by_id!(params[:submission_id])
+    end
   end
 
 end
