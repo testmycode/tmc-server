ef9b1cad4e3efe386da9caefc9e6731e0286fd56
Martin Pärtel <martin.partel@gmail.com>
2014-08-09 20:38:55 +0100

    Made scripts/background_daemon support pluggable background tasks.

---
+++ lib/background_task_registry.rb
@@ -0,0 +1,17 @@
+module BackgroundTaskRegistry
+  def self.register(cls)
+    classes << cls
+  end
+
+  def self.classes
+    @classes ||= []
+  end
+
+  def self.all_tasks
+    @instances ||= classes.map(&:new)
+  end
+end
+
+Dir.glob(Rails::root.to_s + '/app/background_tasks/*.rb').each do |f|
+  BackgroundTaskRegistry.register File.basename(f, '.rb').classify.constantize
+end
