09c8f16627def0601530a6f58672a1b516c4c229
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-11-26 15:51:47 +0200

    Asset improvements

--- app/assets/javascripts/review_notifications.js
+++ app/assets/javascripts/review_notifications.js
@@ -41,4 +41,3 @@ var ReviewNotifications = (function() {
     }
   }
 })();
-
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,2 +1,69 @@
-// ...
-//= require page_presence
+var PagePresence = (function() {
+  var changeCallbacks = [];
+  var initialCallbacks = [];
+  var initial = null;
+
+  CometConnection.connected(function(cometd) {
+    var thisPage = getMeta('current_path');
+    cometd.subscribe('/broadcast/page-presence' + thisPage, pagePresenceUpdate);
+  });
+
+  function pagePresenceUpdate(message) {
+    var users = message.data.users;
+    if (initial == null) {
+      initial = users;
+      fireInitial();
+    }
+
+    fireChange(users);
+  }
+
+  function fireChange(users) {
+    _.each(changeCallbacks, function(callback) {
+      callback(users);
+    });
+  }
+
+  function fireInitial() {
+    _.each(initialCallbacks, function(callback) {
+      callback(initial);
+    });
+    initialCallbacks = [];
+  }
+
+  return {
+    change: function(callback) {
+      changeCallbacks.push(callback);
+      if (initial != null) {
+        callback(initial);
+      }
+    },
+    initial: function(callback) {
+      if (initial == null) {
+        initialCallbacks.push(callback);
+      } else {
+        callback(initial);
+      }
+    }
+  }
+})();
+
+$(document).ready(function() {
+  PagePresence.change(function(users) {
+    var $pp = $('#page-presence');
+    $pp.html('');
+    $pp.append('<span>Users on this page:</span> ');
+    if (users.length <= 1) {
+      $pp.append('<span>just you</span>');
+    } else {
+      _.each(users, function(user, i) {
+        var $user = $('<span class="user"></span>');
+        $user.text(user);
+        $pp.append($user);
+        if (i < users.length - 1) {
+          $pp.append(', ');
+        }
+      });
+    }
+  })
+});
