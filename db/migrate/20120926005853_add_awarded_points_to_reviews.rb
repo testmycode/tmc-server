2ebbf6c669e60c27e1ee4d31650f10f06bee84de
Martin Pärtel <martin.partel@gmail.com>
2014-12-04 01:17:01 +0000

    Wrap migration calls to pg_comment in respond_to? blocks.
    Fixes migrations now that the pg_comment gem has been removed.

--- db/migrate/20120926005853_add_awarded_points_to_reviews.rb
+++ db/migrate/20120926005853_add_awarded_points_to_reviews.rb
@@ -1,7 +1,9 @@
 class AddAwardedPointsToReviews < ActiveRecord::Migration
   def up
     add_column :reviews, :points, :text
-    set_column_comment 'reviews', 'points', 'Space-separated list of points awarded. Does not (generally) contain points already awarded in an earlier review.'
+    if respond_to?(:set_column_comment)
+      set_column_comment 'reviews', 'points', 'Space-separated list of points awarded. Does not (generally) contain points already awarded in an earlier review.'
+    end
   end
   def down
     remove_column :reviews, :points
--- db/migrate/20141124122855_remove_pg_comments.rb
+++ db/migrate/20141124122855_remove_pg_comments.rb
@@ -1,7 +1,9 @@
 class RemovePgComments < ActiveRecord::Migration
   def change
-    remove_table_comment :awarded_points
-    remove_column_comments :reviews, :points
-    remove_column_comments :submissions, :points
+    if respond_to?(:remove_table_comment)
+      remove_table_comment :awarded_points
+      remove_column_comments :reviews, :points
+      remove_column_comments :submissions, :points
+    end
   end
 end
