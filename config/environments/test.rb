2307f7ad3e61e85630584d328eed38c209ed6241
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-12-09 12:57:44 +0200

    Serve asset pipeline in debug mode for tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 end
