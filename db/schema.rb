7551eef9051740d9d17c71a83cee38fd7f64cd99
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-05-18 14:54:28 +0300

    Added CourseTemplate model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222171258) do
+ActiveRecord::Schema.define(version: 20150518095422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 20150222171258) do
     t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "course_templates", force: true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.string   "description"
+    t.string   "material_url"
+    t.string   "source_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "courses", force: true do |t|
---
+++ spec/models/course_template_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe CourseTemplate, type: :model do
+
+  describe 'validation' do
+    let(:valid_params) do
+      {
+        name: 'TestTemplateCourse',
+        source_url: 'git@example.com',
+        title: 'Test Template Title'
+      }
+    end
+
+    it 'requires a name' do
+      should_be_invalid_params(valid_params.merge(name: nil))
+      should_be_invalid_params(valid_params.merge(name: ''))
+    end
+
+    it 'requires name to be reasonably short' do
+      should_be_invalid_params(valid_params.merge(name: 'a' * 41))
+    end
+
+    it 'requires name to be non-unique' do
+      CourseTemplate.create!(valid_params)
+      should_be_invalid_params(valid_params)
+    end
+
+    it 'forbids spaces in the name' do # this could eventually be lifted as long as everything else is made to tolerate spaces
+      should_be_invalid_params(valid_params.merge(name: 'Test Template'))
+    end
+
+    it 'requires a title' do
+      should_be_invalid_params(valid_params.merge(title: nil))
+      should_be_invalid_params(valid_params.merge(title: ''))
+    end
+
+    it 'requires title to be reasonably short' do
+      should_be_invalid_params(valid_params.merge(title: 'a' * 41))
+    end
+
+    it 'requires title to be reasonably long' do
+      should_be_invalid_params(valid_params.merge(title: 'aaa'))
+    end
+
+    it 'requires description to be reasonably short' do
+      should_be_invalid_params(valid_params.merge(description: 'a' * 513))
+    end
+
+    it 'requires a remote repo url' do
+      should_be_invalid_params(valid_params.merge(source_url: nil))
+      should_be_invalid_params(valid_params.merge(source_url: ''))
+    end
+
+    def should_be_invalid_params(params)
+      expect { CourseTemplate.create!(params) }.to raise_error
+    end
+  end
+
+end
