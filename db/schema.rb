0c66f75c5e06f194d80b4ed70fe16a3084479297
jruo <jruo@users.noreply.github.com>
2015-05-19 12:03:07 +0300

    Added organization model
    Conflicts:
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518095422) do
+ActiveRecord::Schema.define(version: 20150518100837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,16 @@ ActiveRecord::Schema.define(version: 20150518095422) do
   end
 
   add_index "feedback_questions", ["id"], name: "index_feedback_questions_on_id", using: :btree
+
+  create_table "organizations", force: true do |t|
+    t.string   "name"
+    t.string   "information"
+    t.string   "slug"
+    t.datetime "accepted_at"
+    t.boolean  "acceptance_pending"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "password_reset_keys", force: true do |t|
     t.integer  "user_id",    null: false
