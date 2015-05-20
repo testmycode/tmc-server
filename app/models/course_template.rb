67d0b1e75351c8c6759ca3d193de94d1721f48dd
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-05-20 14:25:30 +0300

    Rubocop autocorrects added

--- app/models/course_template.rb
+++ app/models/course_template.rb
@@ -1,17 +1,15 @@
 class CourseTemplate < ActiveRecord::Base
-
   validates :name,
             presence: true,
             uniqueness: true,
             length: { within: 1..40 },
             format: {
-                without: / /,
-                message: 'should not contain white spaces'
+              without: / /,
+              message: 'should not contain white spaces'
             }
   validates :source_url, presence: true
   validates :title,
             presence: true,
             length: { within: 4..40 }
   validates :description, length: { maximum: 512 }
-
 end
--- spec/controllers/course_templates_controller_spec.rb
+++ spec/controllers/course_templates_controller_spec.rb
@@ -23,308 +23,284 @@ describe CourseTemplatesController, type: :controller do
     }
   end
 
-  describe "when admin" do
+  describe 'when admin' do
     before :each do
       controller.current_user = @admin
     end
 
-    describe "GET index" do
+    describe 'GET index' do
       before :each do
-        FactoryGirl.create :course_template, name: "template1"
-        FactoryGirl.create :course_template, name: "template2"
-        FactoryGirl.create :course_template, name: "template3"
+        FactoryGirl.create :course_template, name: 'template1'
+        FactoryGirl.create :course_template, name: 'template2'
+        FactoryGirl.create :course_template, name: 'template3'
       end
-      it "should show course templates" do
+      it 'should show course templates' do
         get :index, {}
         expect(assigns(:course_templates).map(&:name)).to eq(%w(template1 template2 template3))
       end
-
     end
 
-    describe "GET show" do
+    describe 'GET show' do
       before :each do
-        @course_template = FactoryGirl.create :course_template, name: "template"
+        @course_template = FactoryGirl.create :course_template, name: 'template'
       end
 
-      it "should show course template" do
-        get :show, {:id => @course_template.to_param}
-        expect(assigns(:course_template).name).to eq("template")
+      it 'should show course template' do
+        get :show, id: @course_template.to_param
+        expect(assigns(:course_template).name).to eq('template')
       end
-
     end
 
-    describe "POST create" do
-      describe "with valid params" do
-        it "creates a new course template" do
-          expect {
-            post :create, {:course_template => valid_attributes}
-          }.to change(CourseTemplate, :count).by(1)
+    describe 'POST create' do
+      describe 'with valid params' do
+        it 'creates a new course template' do
+          expect do
+            post :create, course_template: valid_attributes
+          end.to change(CourseTemplate, :count).by(1)
           expect(CourseTemplate.last.name).to eq('TestTemplateCourse')
         end
 
-        it "redirects to the created course_template" do
-          post :create, {:course_template => valid_attributes}
+        it 'redirects to the created course_template' do
+          post :create, course_template: valid_attributes
           expect(response).to redirect_to(CourseTemplate.last)
         end
       end
 
-      describe "with invalid params" do
+      describe 'with invalid params' do
         it "doesn't create any course templates" do
-          expect {
-            post :create, {:course_template => invalid_attributes}
-          }.to change(CourseTemplate, :count).by(0)
+          expect do
+            post :create, course_template: invalid_attributes
+          end.to change(CourseTemplate, :count).by(0)
         end
 
         it "re-renders the 'new' template" do
-          post :create, {:course_template => invalid_attributes}
-          expect(response).to render_template("new")
+          post :create, course_template: invalid_attributes
+          expect(response).to render_template('new')
         end
       end
     end
 
-    describe "PUT update" do
+    describe 'PUT update' do
       before :each do
-        @course_template = FactoryGirl.create :course_template, name:"oldName"
+        @course_template = FactoryGirl.create :course_template, name: 'oldName'
       end
 
-      describe "with valid params" do
-        it "updates the course template" do
-          put :update, {:id => @course_template.to_param, :course_template => valid_attributes}
+      describe 'with valid params' do
+        it 'updates the course template' do
+          put :update, id: @course_template.to_param, course_template: valid_attributes
           expect(CourseTemplate.last.name).to eq('TestTemplateCourse')
         end
 
-        it "redirects to the updated course template" do
-          put :update, {:id => @course_template.to_param, :course_template => valid_attributes}
+        it 'redirects to the updated course template' do
+          put :update, id: @course_template.to_param, course_template: valid_attributes
           expect(response).to redirect_to(CourseTemplate.last)
         end
       end
 
-      describe "with invalid params" do
+      describe 'with invalid params' do
         it "doesn't update the course template" do
-          put :update, {:id => @course_template.to_param, :course_template => invalid_attributes}
+          put :update, id: @course_template.to_param, course_template: invalid_attributes
           expect(CourseTemplate.last.name).to eq('oldName')
         end
 
         it "re-renders the 'edit' template" do
-          put :update, {:id => @course_template.to_param, :course_template => invalid_attributes}
-          expect(response).to render_template("edit")
+          put :update, id: @course_template.to_param, course_template: invalid_attributes
+          expect(response).to render_template('edit')
         end
       end
-
     end
 
-    describe "DELETE destroy" do
+    describe 'DELETE destroy' do
       before :each do
-        @course_template = FactoryGirl.create :course_template, name:"oldName"
+        @course_template = FactoryGirl.create :course_template, name: 'oldName'
       end
 
-      it "destroys course template" do
-        expect {
-          delete :destroy, {:id => @course_template.to_param}
-        }.to change(CourseTemplate, :count).by(-1)
+      it 'destroys course template' do
+        expect do
+          delete :destroy, id: @course_template.to_param
+        end.to change(CourseTemplate, :count).by(-1)
       end
 
-      it "redirects to the course_templates list" do
-        delete :destroy, {:id => @course_template.to_param}
+      it 'redirects to the course_templates list' do
+        delete :destroy, id: @course_template.to_param
         expect(response).to redirect_to(course_templates_url)
       end
-
     end
   end
 
-
-  describe "when non admin" do
+  describe 'when non admin' do
     before :each do
       controller.current_user = @user
     end
 
-    describe "GET index" do
-
-      it "should respond with a 401" do
+    describe 'GET index' do
+      it 'should respond with a 401' do
         get :index, {}
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "GET show" do
+    describe 'GET show' do
       before :each do
         @course_template = FactoryGirl.create :course_template
       end
 
-      it "should respond with a 401" do
-        get :show, {:id => @course_template.to_param}
+      it 'should respond with a 401' do
+        get :show, id: @course_template.to_param
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "GET new" do
-
-      it "should respond with a 401" do
+    describe 'GET new' do
+      it 'should respond with a 401' do
         get :new, {}
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "GET edit" do
+    describe 'GET edit' do
       before :each do
         @course_template = FactoryGirl.create :course_template
       end
 
-      it "should respond with a 401" do
-        get :edit, {:id => @course_template.to_param}
+      it 'should respond with a 401' do
+        get :edit, id: @course_template.to_param
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "POST create" do
+    describe 'POST create' do
       it "doesn't create any course templates" do
-        expect {
-          post :create, {:course_template => valid_attributes}
-        }.to change(CourseTemplate, :count).by(0)
+        expect do
+          post :create, course_template: valid_attributes
+        end.to change(CourseTemplate, :count).by(0)
       end
 
-      it "should respond with a 401" do
-        post :create, {:course_template => valid_attributes}
+      it 'should respond with a 401' do
+        post :create, course_template: valid_attributes
         expect(response.code.to_i).to eq(401)
       end
     end
 
-    describe "PUT update" do
+    describe 'PUT update' do
       before :each do
-        @course_template = FactoryGirl.create :course_template, name:"oldName"
+        @course_template = FactoryGirl.create :course_template, name: 'oldName'
       end
 
       it "doesn't update the course template" do
-        put :update, {:id => @course_template.to_param, :course_template => invalid_attributes}
+        put :update, id: @course_template.to_param, course_template: invalid_attributes
         expect(CourseTemplate.last.name).to eq('oldName')
       end
 
-      it "should respond with a 401" do
-        put :update, {:id => @course_template.to_param, :course_template => invalid_attributes}
+      it 'should respond with a 401' do
+        put :update, id: @course_template.to_param, course_template: invalid_attributes
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "DELETE destroy" do
+    describe 'DELETE destroy' do
       before :each do
-        @course_template = FactoryGirl.create :course_template, name:"oldName"
+        @course_template = FactoryGirl.create :course_template, name: 'oldName'
       end
 
       it "doesn't destroy the course template" do
-        expect {
-          delete :destroy, {:id => @course_template.to_param}
-        }.to change(CourseTemplate, :count).by(0)
+        expect do
+          delete :destroy, id: @course_template.to_param
+        end.to change(CourseTemplate, :count).by(0)
       end
 
-      it "should respond with a 401" do
-        delete :destroy, {:id => @course_template.to_param}
+      it 'should respond with a 401' do
+        delete :destroy, id: @course_template.to_param
         expect(response.code.to_i).to eq(401)
       end
-
     end
-
   end
 
-  describe "when guest" do
+  describe 'when guest' do
     before :each do
       controller.current_user = @guest
     end
 
-    describe "GET index" do
-
-      it "should respond with a 401" do
+    describe 'GET index' do
+      it 'should respond with a 401' do
         get :index, {}
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "GET show" do
+    describe 'GET show' do
       before :each do
         @course_template = FactoryGirl.create :course_template
       end
 
-      it "should respond with a 401" do
-        get :show, {:id => @course_template.to_param}
+      it 'should respond with a 401' do
+        get :show, id: @course_template.to_param
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "GET new" do
-
-      it "should respond with a 401" do
+    describe 'GET new' do
+      it 'should respond with a 401' do
         get :new, {}
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "GET edit" do
+    describe 'GET edit' do
       before :each do
         @course_template = FactoryGirl.create :course_template
       end
 
-      it "should respond with a 401" do
-        get :edit, {:id => @course_template.to_param}
+      it 'should respond with a 401' do
+        get :edit, id: @course_template.to_param
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "POST create" do
+    describe 'POST create' do
       it "doesn't create any course templates" do
-        expect {
-          post :create, {:course_template => valid_attributes}
-        }.to change(CourseTemplate, :count).by(0)
+        expect do
+          post :create, course_template: valid_attributes
+        end.to change(CourseTemplate, :count).by(0)
       end
 
-      it "should respond with a 401" do
-        post :create, {:course_template => valid_attributes}
+      it 'should respond with a 401' do
+        post :create, course_template: valid_attributes
         expect(response.code.to_i).to eq(401)
       end
     end
 
-    describe "PUT update" do
+    describe 'PUT update' do
       before :each do
-        @course_template = FactoryGirl.create :course_template, name:"oldName"
+        @course_template = FactoryGirl.create :course_template, name: 'oldName'
       end
 
       it "doesn't update the course template" do
-        put :update, {:id => @course_template.to_param, :course_template => invalid_attributes}
+        put :update, id: @course_template.to_param, course_template: invalid_attributes
         expect(CourseTemplate.last.name).to eq('oldName')
       end
 
-      it "should respond with a 401" do
-        put :update, {:id => @course_template.to_param, :course_template => invalid_attributes}
+      it 'should respond with a 401' do
+        put :update, id: @course_template.to_param, course_template: invalid_attributes
         expect(response.code.to_i).to eq(401)
       end
-
     end
 
-    describe "DELETE destroy" do
+    describe 'DELETE destroy' do
       before :each do
-        @course_template = FactoryGirl.create :course_template, name:"oldName"
+        @course_template = FactoryGirl.create :course_template, name: 'oldName'
       end
 
       it "doesn't destroy the course template" do
-        expect {
-          delete :destroy, {:id => @course_template.to_param}
-        }.to change(CourseTemplate, :count).by(0)
+        expect do
+          delete :destroy, id: @course_template.to_param
+        end.to change(CourseTemplate, :count).by(0)
       end
 
-      it "should respond with a 401" do
-        delete :destroy, {:id => @course_template.to_param}
+      it 'should respond with a 401' do
+        delete :destroy, id: @course_template.to_param
         expect(response.code.to_i).to eq(401)
       end
-
     end
-
   end
-
 end
