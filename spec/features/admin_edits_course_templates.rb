4a7fec7d431001332b9e57425e43bc21a2c8082e
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2015-05-26 15:18:11 +0300

    acceptance test for admin edits course templates
    Conflicts:
    	app/controllers/course_templates_controller.rb

---
+++ spec/features/admin_edits_course_templates.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+feature 'Admin edits course templates', feature: true do
+  include IntegrationTestActions
+
+  before :each do
+    @admin = FactoryGirl.create :admin, password: 'xooxer'
+    @user = FactoryGirl.create :user, password: 'foobar'
+
+    FactoryGirl.create :course_template, name: 'dontchange', title: 'dontchange', description: 'dontchange', material_url: 'dontchange', source_url: 'dontchange'
+    FactoryGirl.create :course_template, name: 'oldname', title: 'oldtitle', description: 'olddescription', material_url: 'oldmaterial', source_url: 'oldsource'
+
+    visit '/'
+  end
+
+  scenario 'Admin succeeds at editing' do
+    log_in_as(@admin.login, 'xooxer')
+    visit '/course_templates'
+
+    find('table').find('tr:nth-child(2)').click_link('Edit')
+    fill_in 'course_template_name', with: 'newname'
+    fill_in 'course_template_title', with: 'newtitle'
+    fill_in 'course_template_description', with: 'newdescription'
+    fill_in 'course_template_material_url', with: 'newmaterial'
+    fill_in 'course_template_source_url', with: 'newsource'
+    click_button 'Update Course template'
+
+    expect(page).to have_content('newtitle')
+    expect(page).to have_content('dontchange')
+  end
+
+  scenario 'Non-admin doesnt succeed' do
+    log_in_as(@user.login, 'foobar')
+    visit '/course_templates'
+
+    expect(page).to have_content('Access denied')
+  end
+end
