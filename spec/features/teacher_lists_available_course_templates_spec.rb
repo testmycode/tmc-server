a918fe2e4c59d16770ad011097aa692d0d5e0979
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2015-05-26 12:54:40 +0300

    acceptance test for teachers list course templates

---
+++ spec/features/teacher_lists_available_course_templates_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+feature 'Teacher lists available course templates', feature: true do
+  include IntegrationTestActions
+
+  before :each do
+    @organization = FactoryGirl.create :accepted_organization, slug: 'slug'
+    @teacher = FactoryGirl.create :user, password: 'xooxer'
+    @user = FactoryGirl.create :user, password: 'foobar'
+    Teachership.create! user: @teacher, organization: @organization
+
+    FactoryGirl.create :course_template, title: 'template1'
+    FactoryGirl.create :course_template, title: 'template2'
+    FactoryGirl.create :course_template, title: 'template3'
+
+    visit '/'
+  end
+
+  scenario 'Teacher succeeds at listing course templates' do
+    log_in_as(@teacher.login, 'xooxer')
+
+    visit '/org/slug'
+    click_link 'Create course from template'
+    expect(page).to have_content('template1')
+    expect(page).to have_content('template2')
+    expect(page).to have_content('template3')
+  end
+
+  scenario 'Non-teacher doesnt succeed at listing course templates' do
+    log_in_as(@user.login, 'foobar')
+
+    visit '/org/slug'
+    expect(page).not_to have_content('Create course from template')
+  end
+end
