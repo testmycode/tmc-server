49b365e28c81c38ea9d9a34dac53e6be857d1cbf
termanty <termanty@cs.helsinki.fi>
2015-05-27 15:27:36 +0300

    acceptance test for teacher creates organization

---
+++ spec/features/teacher_creates_organization.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+feature 'User can create new organization', feature: true do
+  include IntegrationTestActions
+
+  before :each do
+    @admin = FactoryGirl.create :admin, password: 'foobar'
+    @user = FactoryGirl.create :user, password: 'foobar2'
+    visit '/org'
+  end
+
+  scenario 'User succeeds create organization with valid parameters' do
+    log_in_as(@user.login, 'foobar2')
+    click_link 'Request a new organization'
+    fill_in 'organization[name]', with: 'Code School'
+    fill_in 'organization[information]', with: 'Learning for real nerds'
+    fill_in 'organization[slug]', with: 'cos'
+    click_button 'Request organization'
+    expect(page).to have_content 'Organization was successfully requested.'
+  end
+
+  scenario 'User cannot create organization with invalid parameters' do
+    log_in_as(@user.login, 'foobar2')
+    click_link 'Request a new organization'
+    fill_in 'organization[name]', with: 'Code School'
+    fill_in 'organization[information]', with: 'Learning for real nerds'
+    fill_in 'organization[slug]', with: 'co.eu'
+    click_button 'Request organization'
+    expect(page).to have_content 'error'
+  end
+
+  scenario 'Admin can accept pending organization creation request' do
+    @organization = FactoryGirl.create :organization
+    Teachership.create!(user: @user, organization: @organization)
+    log_in_as(@admin.login, 'foobar')
+    click_link 'Show'
+    expect(page).to have_content @organization.name
+    click_link 'Accept'
+    expect(page).to have_content 'Organization request was successfully accepted.'
+  end
+
+  scenario 'Admin can reject pending organization creation request' do
+    @organization = FactoryGirl.create :organization
+    Teachership.create!(user: @user, organization: @organization)
+    log_in_as(@admin.login, 'foobar')
+    click_link 'Show'
+    expect(page).to have_content @organization.name
+    click_link 'Reject'
+    expect(page).to have_content "You are about to reject organization #{@organization.name}"
+    click_button 'Reject organization'
+    expect(page).to have_content 'Organization request was successfully rejected.'
+  end
+end
