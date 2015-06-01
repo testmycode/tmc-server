c9df9ee7d5c08bebc739e0bb713320742df9a5e0
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2015-06-01 14:26:49 +0300

    merged migration files

--- db/migrate/20150521091401_make_course_template_name_unique.rb
+++
@@ -1,5 +0,0 @@
-class MakeCourseTemplateNameUnique < ActiveRecord::Migration
-  def change
-    add_index :course_templates, :name, unique: true
-  end
-end
--- db/migrate/20150526082930_add_rejection_information_to_organization.rb
+++
@@ -1,6 +0,0 @@
-class AddRejectionInformationToOrganization < ActiveRecord::Migration
-  def change
-    add_column :organizations, :rejected, :boolean, default: false, null: false
-    add_column :organizations, :rejected_reason, :string
-  end
-end
