513ac6ef70eafabfe1d10d4f4c4ec10a7daaff24
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2015-06-01 14:26:49 +0300

    merged migration files
    Conflicts:
    	db/migrate/20150518095422_create_course_templates.rb

--- db/migrate/20150521092056_make_organization_name_and_slug_unique.rb
+++
@@ -1,6 +0,0 @@
-class MakeOrganizationNameAndSlugUnique < ActiveRecord::Migration
-  def change
-    add_index :organizations, :name, unique: true
-    add_index :organizations, :slug, unique: true
-  end
-end
