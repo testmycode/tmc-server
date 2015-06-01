42e5686bd67f9e703d369a44dedaaee14515f8c2
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2015-06-01 15:41:56 +0300

    course template validation via cloning

--- app/models/course_template.rb
+++ app/models/course_template.rb
@@ -3,6 +3,8 @@
 require 'net/http'
 
 class CourseTemplate < ActiveRecord::Base
+  include SystemCommands
+
   validates :name,
             presence: true,
             uniqueness: true,
@@ -19,9 +21,21 @@ class CourseTemplate < ActiveRecord::Base
   validate :valid_source_url?
 
   def valid_source_url?
-    url = URI(source_url)
-    Net::HTTP.get(url)
+    FileUtils.rm_rf(clone_validation_path)
+    FileUtils.mkdir_p(clone_validation_path)
+    sh!('git', 'clone', '-q', '-b', 'master', self.source_url, clone_validation_path)
+    if File.exist?("#{clone_validation_path}/.git")
+      FileUtils.rm_rf(clone_validation_path)
+      return true
+    end
+    FileUtils.rm_rf(clone_validation_path)
+    errors.add(:source_url, 'is invalid: Could not clone git repo')
   rescue StandardError => e
+    FileUtils.rm_rf(clone_validation_path)
     errors.add(:source_url, 'is invalid: ' + e.to_s)
+  end
+
+  def clone_validation_path
+    "#{FileStore.root}/validation"
   end
 end
