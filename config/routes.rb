33d82454b3b0579d76acddc09a1def1ead32fdd1
termanty <termanty@cs.helsinki.fi>
2015-06-03 15:12:20 +0300

    Admin can toggle course templates visibility

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,11 @@ TmcServer::Application.routes.draw do
     end
   end
 
-  resources :course_templates, except: :show
+  resources :course_templates, except: :show do
+    member do
+      post 'toggle_hidden', to: 'course_templates#toggle_hidden'
+    end
+  end
 
   resources :sessions, only: [:new, :create, :destroy]
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602104502) do
+ActiveRecord::Schema.define(version: 20150603080807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150602104502) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "expires_at"
+    t.boolean  "hidden",       default: false
   end
 
   add_index "course_templates", ["name"], name: "index_course_templates_on_name", unique: true, using: :btree
