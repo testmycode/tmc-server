3a305bd6f1e6dea0c3eb05b17d0f2d60d195bc10
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2015-06-03 11:00:43 +0300

    db migration handled so courses arent left as orphans

--- db/migrate/20150527103134_course_belongs_to_organization.rb
+++ db/migrate/20150527103134_course_belongs_to_organization.rb
@@ -1,5 +1,27 @@
 class CourseBelongsToOrganization < ActiveRecord::Migration
   def change
     add_reference :courses, :organization, index: true, foreign_key: true
+    default_organization = Organization.find_by slug: 'default'
+    reversible do |dir|
+      dir.up do
+        orphans = Course.select { |c| c.organization.nil? }
+        unless orphans.empty?
+          if default_organization.nil?
+            default_organization = Organization.create name: 'Default',
+                                               information: 'Temporary organization used for migrating purposes',
+                                               slug: 'default',
+                                               acceptance_pending: false
+          end
+          orphans.each do |c|
+            c.organization = default_organization
+            c.save
+          end
+        end
+      end
+      dir.down do
+        Course.all.each { |c| c.organization = nil }
+        default_organization.destroy unless default_organization.nil?
+      end
+    end
   end
 end
