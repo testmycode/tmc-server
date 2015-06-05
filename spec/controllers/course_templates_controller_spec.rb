ee1f56a28312dea81e833e3a293637142a4b0528
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-06-05 16:21:39 +0300

    Fixed source url test with valid values

--- spec/controllers/course_templates_controller_spec.rb
+++ spec/controllers/course_templates_controller_spec.rb
@@ -334,13 +334,13 @@ describe CourseTemplatesController, type: :controller do
 
     describe 'GET prepare_course' do
       before :each do
-        @template = FactoryGirl.create :course_template, name: 'name', source_url: 'http://example.com'
+        @template = FactoryGirl.create :course_template, name: 'name', source_url: 'https://github.com/testmycode/tmc-testcourse.git'
       end
 
       it 'should assign @course with course template attributes' do
         get :prepare_course, organization_id: @organization.slug, id: @template.id
         expect(assigns(:course).name).to eq('name')
-        expect(assigns(:course).source_url).to eq('http://example.com')
+        expect(assigns(:course).source_url).to eq('https://github.com/testmycode/tmc-testcourse.git')
       end
     end
   end
