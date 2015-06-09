eb6d2e25396dbdf8f5cb04cea575674d673ac9bd
Janne Pakarinen <janne.pakarinen@helsinki.fi>
2015-06-09 14:45:48 +0300

    Added test for teacher seeing submissions

--- spec/features/teacher_has_admin_abilities_to_own_course_spec.rb
+++ spec/features/teacher_has_admin_abilities_to_own_course_spec.rb
@@ -7,9 +7,13 @@ feature 'Teacher has admin abilities to own course', feature: true do
     @organization = FactoryGirl.create(:accepted_organization, slug: 'slug')
     @teacher = FactoryGirl.create :user, password: 'xooxer'
     Teachership.create! user: @teacher, organization: @organization
+    @user = FactoryGirl.create :user, password: 'foobar'
 
     @course = Course.create!(name: 'mycourse', source_backend: 'git', source_url: 'https://github.com/testmycode/tmc-testcourse.git', organization: @organization)
     @course.refresh
+
+    @submission = FactoryGirl.create :submission, course: @course, user: @user, exercise_name: 'trivial'
+    @submission_data = FactoryGirl.create :submission_data, submission: @submission
 
     visit '/'
     log_in_as(@teacher.login, 'xooxer')
@@ -23,5 +27,20 @@ feature 'Teacher has admin abilities to own course', feature: true do
 
     expect(page).to have_content('Solution for arith_funcs')
     expect(page).to have_content('src/Arith.java')
+  end
+
+  scenario 'Teacher can see all submissions for his organizations courses' do
+    visit '/org/slug/courses/1'
+
+    expect(page).to have_content('Latest submissions')
+    expect(page).not_to have_content('No data available in table')
+    expect(page).to have_content('Showing 1 to 1 of 1 entries')
+
+    click_link('Details')
+
+    expect(page).to have_content('Submission 1')
+    expect(page).to have_content('Submitted at')
+    expect(page).to have_content('Test Results')
+    expect(page).not_to have_content('Access denied')
   end
 end
