f2f21ba601a8368b25f0af2a1393801ab3cab184
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2015-06-12 11:36:53 +0300

    added parameters can be copied from template

--- spec/features/teacher_creates_course_from_template_spec.rb
+++ spec/features/teacher_creates_course_from_template_spec.rb
@@ -21,10 +21,15 @@ feature 'Teacher creates course from course template', feature: true do
     click_link 'Create New Course from template'
     click_link 'Create course'
     fill_in 'course_name', with: 'customname'
+    fill_in 'course_title', with: 'Custom Title'
+    fill_in 'course_description', with: 'Custom description'
+    fill_in 'course_material_url', with: 'custommaterial.com'
     click_button 'Add Course'
 
     expect(page).to have_content('Course was successfully created')
-    expect(page).to have_content('customname')
+    expect(page).to have_content('Custom Title')
+    expect(page).to have_content('Custom description')
+    expect(page).to have_link('http://custommaterial.com')
     expect(page).to have_content('help page')
 
     click_link 'View status page'
@@ -32,7 +37,7 @@ feature 'Teacher creates course from course template', feature: true do
     expect(page).to have_content('maven_exercise')
 
     visit '/org/slug/courses'
-    expect(page).to have_content('customname')
+    expect(page).to have_content('Custom Title')
   end
 
   scenario 'Teacher doesnt succeed with invalid parameters' do
