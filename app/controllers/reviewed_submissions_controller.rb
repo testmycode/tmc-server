a0e53e2f7c7f5a368a3c0b885e02ec9b92dfb1f9
termanty <termanty@cs.helsinki.fi>
2015-06-23 13:55:36 +0300

    review cleaning and updating breadcrumbs
    Conflicts:
    	app/controllers/reviewed_submissions_controller.rb

--- app/controllers/reviewed_submissions_controller.rb
+++ app/controllers/reviewed_submissions_controller.rb
@@ -1,23 +1,18 @@
-# Shows an admin the list of submissions that have already been reviewed.
+# Shows for admin the list of submissions that have already been reviewed.
 class ReviewedSubmissionsController < ApplicationController
-  before_action :set_organization
-
+  
   def index
     @course = Course.find(params[:course_id])
-    authorize! :list_code_reviews, @course
+    @organization = @course.organization
+    authorize! :teach, @course
     add_course_breadcrumb
-    add_breadcrumb 'Reviewed submissions', organization_course_reviewed_submissions_path
+    add_breadcrumb 'Code reviews', organization_course_reviews_path(@organization, @course)
+    add_breadcrumb 'Reviewed submissions'
 
     @submissions = @course.submissions
       .where(reviewed: true)
       .includes(reviews: :reviewer)
       .includes(:user)
       .order('created_at DESC')
-  end
-
-  private
-
-  def set_organization
-    @organization = Organization.find_by(slug: params[:organization_id])
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,7 +16,7 @@ class ReviewsController < ApplicationController
       respond_to do |format|
         format.html do
           add_course_breadcrumb
-          add_breadcrumb 'Code reviews', organization_course_reviews_path
+          add_breadcrumb 'Code reviews'
           render 'reviews/course_index'
         end
         format.json do
@@ -51,9 +51,8 @@ class ReviewsController < ApplicationController
     @course = @submission.course
     @organization = @course.organization
     add_course_breadcrumb
-    add_exercise_breadcrumb
-    add_submission_breadcrumb
-    add_breadcrumb 'Code review editor', new_submission_review_path(@submission)
+    add_breadcrumb 'Code reviews', organization_course_reviews_path(@organization, @course)
+    add_breadcrumb 'Code review editor'
 
     @new_review = Review.new(
       submission_id: @submission.id,
