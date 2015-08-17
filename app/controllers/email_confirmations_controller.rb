825352df8cef7300f6bb782d1afeb8390676d4c7
termanty <termanty@cs.helsinki.fi>
2015-08-17 10:38:08 +0300

    custom session for email confirmation request
    Conflicts:
    	app/controllers/email_confirmations_controller.rb

--- app/controllers/email_confirmations_controller.rb
+++ app/controllers/email_confirmations_controller.rb
@@ -13,14 +13,15 @@ class EmailConfirmationsController < ApplicationController
   end
 
   def request_user_to_confirm_email
-    @user = User.find_by(login: params[:username])
+    @user = user_from_non_confirmed_session
     render 'email_confirmations/email_confirmation_request'
   end
 
   def send_confirmation_mail
-    user = User.find_by(login: params[:username])
+    user = user_from_non_confirmed_session
     token = ActionToken.generate_email_confirmation_token(user)
     EmailConfirmationMailer.confirmation_link_email(user, token).deliver
+    clear_non_confirmed_session_for user
     redirect_to root_path, notice: 'Check your emails and click the confirmation link of the email we send'
   end
 end
