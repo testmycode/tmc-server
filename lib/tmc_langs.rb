e1b0092960f4241eebdfcb9ac053620dc89d5fd4
Jarmo Isotalo <jamo@isotalo.fi>
2015-11-11 22:11:19 +0200

    Start using tmc-langs

---
+++ lib/tmc_langs.rb
@@ -0,0 +1,30 @@
+require 'system_commands'
+require 'json'
+require 'tempfile'
+require 'maven_project'
+
+# Interface to compiling tmc-comet. See also: models/comet_server.rb
+class TmcLangs < MavenProject
+  def self.get
+    @instance ||= TmcLangs.new
+  end
+
+  # Get instances via .get instead
+  def initialize
+    super("#{::Rails.root}/ext/tmc-langs")
+  end
+
+  def pom_path
+    path + 'tmc-langs-cli/' +  'pom.xml'
+  end
+
+  def jar_and_lib_paths
+    [jar_path] + lib_paths
+  end
+
+  def find_exercise_dirs(path)
+    temp_file = ::Tempfile.new('langs')
+    SystemCommands.sh!('java', '-jar', jar_path, 'find-exercises', path, temp_file.path)
+    JSON.parse(File.read(temp_file))
+  end
+end
