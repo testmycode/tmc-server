a97d31608d353bad0c8b910e27bcf6c9c0188059
Jarmo Isotalo <jamo@isotalo.fi>
2015-12-03 23:57:50 +0200

    New version for each travis run

--- .travis-update-usermanual.sh
+++ .travis-update-usermanual.sh
@@ -3,7 +3,8 @@
 
 git clone https://github.com/testmycode-usermanual/testmycode-usermanual.github.io.git
 cd testmycode-usermanual.github.io
-cp -R ../doc/usermanual .
+mkdir "$TRAVIS_BUILD_ID"
+cp -R ../doc/usermanual  "$TRAVIS_BUILD_ID"
 git add -A
 git commit -m "Add built usermanual to usermanual repo for "
 git push --force "https://testmycode-usermanual:$GITHUB_API_KEY@github.com/testmycode-usermanual/testmycode-usermanual.github.io" master:master 2>&1 | sed "s/$GITHUB_API_KEY/<confidential>/g"
