2e763fbc1e4aea6a8bd9a17c757bb36999ac8f13
= <jamo@isotalo.fi>
2016-01-29 11:57:34 +0000

    Restore use of configurable cookie name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :active_record_store, key: '_tmc_server_session'
+Rails.application.config.session_store :active_record_store, key: SiteSetting.value(:session_cookie_key), httponly: false
