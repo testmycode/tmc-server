1a2d43a3215a3fba1deca20dd369a2b7644e9717
Jarmo Isotalo <jamo@isotalo.fi>
2016-01-31 17:40:01 +0200

    Show .py files and update langs

--- app/models/source_file_list.rb
+++ app/models/source_file_list.rb
@@ -132,6 +132,7 @@ class SourceFileList
       name.end_with?('.hpp') ||
       name.end_with?('.h') ||
       name.end_with?('.rb') ||
+      (dir.include?('/src') && name.end_with?('.py')) ||
       dir.include?('/WEB-INF')
   end
 
