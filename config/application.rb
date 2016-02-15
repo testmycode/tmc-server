45e915b21c271a8e211e8d2f4b7aed2c971a9b61
Jarmo Isotalo <jamo@isotalo.fi>
2016-02-15 13:25:30 +0200

    Allow oauth thru cors

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module TmcServer
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
         origins SiteSetting.all_settings['cors_origins']
+        resource '/oauth*', headers: :any, methods: :any
         resource '/api*', headers: :any, methods: [:get, :post]
         resource '/auth*', headers: :any, methods: [:get, :post]
         resource '/paste/*', headers: :any, methods: [:get]
