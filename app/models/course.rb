9c9618469715bdca7caf16c5d623b6377d95df1c
Tmc Server <tmc@tmc.mooc.fi>
2016-02-29 13:35:04 +0000

    Skip courses with incorrect unlock spec

--- app/models/course.rb
+++ app/models/course.rb
@@ -254,11 +254,16 @@ class Course < ActiveRecord::Base
   end
 
   def certificate_downloadable_for?(user)
-    user.administrator? || (
-      !user.guest? &&
-       certificate_downloadable &&
-       (certificate_unlock_spec.nil? ||
-        UnlockSpec.new(self, ActiveSupport::JSON.decode(certificate_unlock_spec)).permits_unlock_for?(user)))
+    begin 
+      return user.administrator? || (
+        !user.guest? &&
+         certificate_downloadable &&
+         (certificate_unlock_spec.nil? ||
+          UnlockSpec.new(self, ActiveSupport::JSON.decode(certificate_unlock_spec)).permits_unlock_for?(user)))
+    rescue => e
+      Rails.logger.warn("Incorrect certificate download spec: #{e}")
+      return false
+    end
   end
 
   # Returns a hash of exercise group => {
