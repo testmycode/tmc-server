e4f634e47ce421f40649e4a36f4d0e9ee7025c33
Jarmo Isotalo <jamo@isotalo.fi>
2016-03-25 14:31:14 +0200

    Dump schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160310111733) do
-=======
-ActiveRecord::Schema.define(version: 20160214132532) do
->>>>>>> Added doorkeeper
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
