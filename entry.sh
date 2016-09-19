62e7458c59f5d4d78d33e6d90df0f2789f1141ba
Jarmo Isotalo <jamo@isotalo.fi>
2016-09-19 15:36:40 +0300

    Hax sandbox to use proper hostname in tests

--- entry.sh
+++ entry.sh
@@ -10,4 +10,16 @@ done
 if [ ! -z $cmd ]; then
  exec $cmd
 fi
-bundle exec rake db:create db:migrate spec SPEC_OPTS="--pattern $RSPEC_PATTERN --tag ~network --format RspecRemoteFormatter "
+
+echo "Cleaning customizations for hermetic stable test setup"
+rm -f config/site.yml
+echo "Fixing site.yml for test"
+sed -i 's/\(baseurl_for_remote_sandboxes:\).*/\1 '"$HOST"'/' config/site.defaults.yml
+
+echo "Running migrations and starting tests"
+
+if [ ! -z $REPORT_URL ]; then
+  bundle exec rake db:create db:migrate spec SPEC_OPTS="--pattern $RSPEC_PATTERN --tag ~network --format RspecRemoteFormatter "
+else
+  bundle exec rake db:create db:migrate spec SPEC_OPTS="--pattern $RSPEC_PATTERN --tag ~network -f d "
+fi
