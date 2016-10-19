983f3a7d8667b5ce0e371887cc59545de088e99e
Antti Leinonen <antti.leinonen@helsinki.fi>
2016-10-19 10:28:07 +0300

    Add v8 apidocs controller to routes

--- app/controllers/api/v8/apidocs_controller.rb
+++ app/controllers/api/v8/apidocs_controller.rb
@@ -2,58 +2,58 @@ class Api::V7::ApidocsController < ActionController::Base
   include Swagger::Blocks
 
   swagger_root do
-    key :swagger, '2.0'
+    key :swagger, "2.0"
     info do
-      key :version, '1.0.0'
-      key :title, 'TMC API documentation'
-      key :description, 'TMC API documentation'
+      key :version, "1.0.0"
+      key :title, "TMC API documentation"
+      key :description, "TMC API documentation"
       contact do
-        key :name, 'TMC API Team'
-        key :url, 'https://cs.helsinki.fi'
+        key :name, "TestMyCode"
+        key :url, "https://cs.helsinki.fi"
       end
       license do
-        key :name, 'MIT'
+        key :name, "MIT"
       end
     end
     tag do
-      key :name, 'api'
-      key :description, 'API operations'
+      key :name, "api"
+      key :description, "API operations"
     end
     parameter :path_organization_id do
       key :name, :organization_id
       key :in, :path
-      key :description, 'Organization\'s id'
+      key :description, "Organization's id"
       key :required, true
       key :type, :string
     end
     parameter :path_course_id do
       key :name, :course_id
       key :in, :path
-      key :description, 'Course\'s id'
+      key :description, "Course's id"
       key :required, true
       key :type, :integer
     end
     parameter :path_exercise_id do
       key :name, :exercise_id
       key :in, :path
-      key :description, 'Exercise\'s id'
+      key :description, "Exercise's id"
       key :required, true
       key :type, :integer
     end
     response :error do
-      key :description, 'An error occurred'
+      key :description, "An error occurred"
       schema do
         key :title, :errors
-        key :description, 'A list of error messages'
+        key :description, "A list of error messages"
         key :type, :array
         items do
           key :type, :string
         end
       end
     end
-    key :host, 'localhost:3000'
-    key :consumes, ['application/json']
-    key :produces, ['application/json']
+    key :host, "localhost:3000"
+    key :consumes, ["application/json"]
+    key :produces, ["application/json"]
   end
 
   # A list of all classes that have swagger_* declarations.
