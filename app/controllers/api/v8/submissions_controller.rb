bd6fea5077653a006b8ed80b1c967ef95911bbb7
Antti Leinonen <antti.leinonen@helsinki.fi>
2016-10-25 11:39:26 +0300

    Method for finding all submissions by course name

--- app/controllers/api/v8/submissions_controller.rb
+++ app/controllers/api/v8/submissions_controller.rb
@@ -1,5 +1,13 @@
 class Api::V8::SubmissionsController < ApplicationController # ApplicationController --> BaseController sit ku PR merged
-  def index
-    
+  def all_submissions
+    @course = Course.find_by(name: params[:course_name])
+    authorize! :read, @course
+    @submissions = Submission.find_by(course_id: @course.id)
+    authorize! :read, @submissions
+
+    render json: {
+      api_version: ApiVersion::API_VERSION,
+      submissions: @submissions
+    }
   end
 end
