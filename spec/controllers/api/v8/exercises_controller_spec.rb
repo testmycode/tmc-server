49c9f1247f092a86234c4f98764b9819d136131b
Jacinto Ramirez Lahti <jacinto.ramirez.lahti@cs.helsinki.fi>
2016-10-25 17:00:28 +0300

    Cleaned up the spec for the exercise search api end point

--- spec/controllers/api/v8/exercises_controller_spec.rb
+++ spec/controllers/api/v8/exercises_controller_spec.rb
@@ -1,31 +1,29 @@
 require 'spec_helper'
-require 'pp'
 
 describe Api::V8::ExercisesController, type: :controller do
+  let(:organization) { FactoryGirl.create(:accepted_organization, slug: 'slug') }
+  let(:course) { FactoryGirl.create(:course, organization: organization) }
+  let(:exercise) { FactoryGirl.create(:exercise, course: course) }
+  let(:available_point) { FactoryGirl.create(:available_point, exercise: exercise) }
+  let(:admin) { FactoryGirl.create(:admin, password: 'xooxer') }
+  let(:user) { FactoryGirl.create(:user, login: 'user', password: 'xooxer') }
 
-  before do
-    @organization = FactoryGirl.create(:accepted_organization, slug: 'slug')
-    @course = FactoryGirl.create(:course, organization: @organization)
-    @exercise = FactoryGirl.create(:exercise, course: @course)
-    @available_point = FactoryGirl.create(:available_point, exercise: @exercise)
-    @admin = FactoryGirl.create(:admin, password: 'xooxer')
-    @user = FactoryGirl.create(:user, login: 'user', password: 'xooxer')
-
+  before :each do
     controller.stub(:doorkeeper_token) { token }
   end
 
   describe 'As an admin' do
-    let(:token) { double resource_owner_id: @admin.id, acceptable?: true }
-      it 'should get json response of the courses exercises' do
-        get :index, { id: @course.id }
-        expect(response).to have_http_status(:success)
-      end
+    let(:token) { double resource_owner_id: admin.id, acceptable?: true }
+    it 'should get json response of the courses exercises' do
+      get :index, { id: course.id }
+      expect(response).to have_http_status(:success)
+    end
   end
 
   describe 'As a student' do
-    let(:token) { double resource_owner_id: @user.id, acceptable?: true }
+    let(:token) { double resource_owner_id: user.id, acceptable?: true }
     it 'should get json response of the courses exercises' do
-      get :index, { id: @course.id }
+      get :index, { id: course.id }
       expect(response).to have_http_status(:success)
     end
   end
@@ -33,7 +31,7 @@ describe Api::V8::ExercisesController, type: :controller do
   describe 'As an unauthorized user' do
     let(:token) { double resource_owner_id: 1234, acceptable?: true }
     it 'should not work' do
-      expect { get :index, { id: @course.id } }.to raise_error
+      expect { get :index, { id: course.id } }.to raise_error
     end
   end
 end
