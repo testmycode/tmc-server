c364713737034375df5533ec725b8c6920a0e562
Jacinto Ramirez Lahti <jacinto.ramirez.lahti@cs.helsinki.fi>
2016-11-21 13:27:17 +0200

    Modify documentation for user's basic info end point

--- app/controllers/api/v8/users_controller.rb
+++ app/controllers/api/v8/users_controller.rb
@@ -11,14 +11,6 @@ class Api::V8::UsersController < Api::V8::BaseController
       key :tags, [
           'user'
       ]
-      response 403, '$ref': '#/responses/auth_required'
-      response 404 do
-        key :description, 'User not found'
-        schema do
-          key :title, :errors
-          key :type, :json
-        end
-      end
       response 200 do
         key :description, 'User\'s username and email as json'
         schema do
@@ -29,6 +21,8 @@ class Api::V8::UsersController < Api::V8::BaseController
           end
         end
       end
+      response 403, '$ref': '#/responses/error'
+      response 404, '$ref': '#/responses/error'
     end
   end
 
@@ -43,14 +37,6 @@ class Api::V8::UsersController < Api::V8::BaseController
           'user'
       ]
       parameter '$ref': '#/parameters/path_user_id'
-      response 403, '$ref': '#/responses/auth_required'
-      response 404 do
-        key :description, 'User not found'
-        schema do
-          key :title, :errors
-          key :type, :json
-        end
-      end
       response 200 do
         key :description, 'User\'s username and email by id as json'
         schema do
@@ -61,6 +47,8 @@ class Api::V8::UsersController < Api::V8::BaseController
           end
         end
       end
+      response 403, '$ref': '#/responses/error'
+      response 404, '$ref': '#/responses/error'
     end
   end
 
