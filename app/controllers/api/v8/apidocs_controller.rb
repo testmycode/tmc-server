fb10113628210525ef52b975d45bc059e917e805
Jacinto Ramirez Lahti <jacinto.ramirez.lahti@cs.helsinki.fi>
2016-11-22 18:03:41 +0200

    Modify ApiDocs to us correct namespace for user basic info

--- app/controllers/api/v8/apidocs_controller.rb
+++ app/controllers/api/v8/apidocs_controller.rb
@@ -93,7 +93,7 @@ class Api::V8::ApidocsController < ActionController::Base
     Api::V8::CoursesController,
     Api::V8::ExercisesController,
     Api::V8::SubmissionsController,
-    Api::V8::UsersController,
+    Api::V8::Users::BasicInfoController,
     Course,
     Exercise,
     AvailablePoint,
