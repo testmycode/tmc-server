b2ae65bd17eeb27ecbb730a3370b31bb9a4c1ca2
makevertti <makevertti@users.noreply.github.com>
2016-12-05 15:43:14 +0200

    tests for core exercises controller

--- spec/controllers/api/v8/core/exercises_controller_spec.rb
+++ spec/controllers/api/v8/core/exercises_controller_spec.rb
@@ -42,6 +42,35 @@ describe Api::V8::Core::ExercisesController, type: :controller do
     end
   end
 
+  describe 'When logged in' do
+    let(:token) { double resource_owner_id: user.id, acceptable?: true }
+    describe 'and correct exercise id is given' do
+      it 'should return correct data' do
+        get :show, id: exercise.id
+        expect(response.body).to include course.name
+        expect(response.body).to include 'submissions'
+        expect(response.body).to include 'http://test.host/submissions/1.zip'
+      end
+    end
+    describe 'and invalid exercise id is given' do
+      it 'should show appropriate error' do
+        get :show, id: 741852963
+        expect(response).to have_http_status :not_found
+        expect(response.body).to include "Couldn't find Exercise with 'id'=741852963"
+      end
+    end
+  end
+
+  describe 'When unauthenticated' do
+    let(:current_user) { Guest.new }
+    let(:token) { nil }
+    it 'should show authentication error' do
+      get :show, id: exercise.id
+      expect(response).to have_http_status :forbidden
+      expect(response.body).to include 'Authentication required'
+    end
+  end
+
   after :all do
     FileUtils.rm_rf(repo_path)
   end
