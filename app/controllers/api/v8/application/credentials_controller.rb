08986b3c2a0d584aa3ddd4ba368f989e9ff77548
Antti Leinonen <antti.leinonen@helsinki.fi>
2017-04-06 13:39:57 +0300

    Fix credentials view to show application id properly

--- app/controllers/api/v8/application/credentials_controller.rb
+++ app/controllers/api/v8/application/credentials_controller.rb
@@ -9,7 +9,7 @@ module Api
           return unless valid_clients.is_a?(Enumerable)
           return render json: 'Client not supported!', status: :forbidden unless valid_clients.include?(params[:application_name])
           app = Doorkeeper::Application.create_with(redirect_uri: 'urn:ietf:wg:oauth:2.0:oob').find_or_create_by!(name: params[:application_name])
-          present(application_id: app.name, secret: app.secret)
+          present(application_id: app.uid, secret: app.secret)
         end
       end
     end
