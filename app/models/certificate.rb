4f8694499fbf056c2346aaae7899ef842817f952
Henrik Nygren <nygrenh@gmail.com>
2017-07-03 14:08:11 +0300

    Do not include hidden points in cetificates

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -44,7 +44,7 @@ class Certificate < ActiveRecord::Base
 
   def visible_exercises_and_points_for
     visible_exercises = course.exercises.select { |e| e.points_visible_to?(user) }
-    total_available = AvailablePoint.course_points_of_exercises(course, visible_exercises)
+    total_available = AvailablePoint.course_points_of_exercises(course, visible_exercises, hidden: true)
     [visible_exercises, total_available]
   end
 end
