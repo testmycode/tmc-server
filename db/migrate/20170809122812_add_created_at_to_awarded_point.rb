5686d9fad37a145aedd799d69e963132ca9f7da2
Juuha <juha.ritakoski@hotmail.com>
2017-08-11 15:44:06 +0300

    Make requested changes

--- db/migrate/20170809122812_add_created_at_to_awarded_point.rb
+++ db/migrate/20170809122812_add_created_at_to_awarded_point.rb
@@ -1,8 +1,8 @@
 class AddCreatedAtToAwardedPoint < ActiveRecord::Migration
   def change
     add_column :awarded_points, :created_at, :datetime
-    AwardedPoint.all.each do |awarded_point|
-      awarded_point.update_attributes! :created_at => awarded_point.submission.created_at
+    AwardedPoint.find_each do |awarded_point|
+      awarded_point.update!(created_at: awarded_point.submission.created_at)
     end
   end
 end
--- db/migrate/20170809125714_add_created_at_to_awarded_point.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatedAtToAwardedPoint < ActiveRecord::Migration
-  def change
-    add_column(:awarded_points, :created_at, :datetime)
-  end
-end
