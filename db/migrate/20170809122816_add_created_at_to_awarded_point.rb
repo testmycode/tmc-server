f58c3fb4b7914c117706fba2ee78b1271a6737de
Juuha <juha.ritakoski@hotmail.com>
2017-08-11 18:19:28 +0300

    Change created_at attribute creation procedure for awarded point to not point to a nil value, when the submission related to the awarded point has been deleted

--- db/migrate/20170809122816_add_created_at_to_awarded_point.rb
+++ db/migrate/20170809122816_add_created_at_to_awarded_point.rb
@@ -2,7 +2,7 @@ class AddCreatedAtToAwardedPoint < ActiveRecord::Migration
   def change
     add_column :awarded_points, :created_at, :datetime
     AwardedPoint.find_each do |awarded_point|
-      awarded_point.update!(created_at: awarded_point.submission.created_at)
+      awarded_point.update!(created_at: awarded_point.submission.created_at) if awarded_point.submission
     end
   end
 end
