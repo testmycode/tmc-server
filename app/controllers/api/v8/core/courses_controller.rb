7d3d09e6ca4e3dfd3053966bdbd3d397bd722235
Henrik Nygren <nygrenh@gmail.com>
2018-01-13 17:18:24 +0200

    Move client blacklist to courses endpoint

--- app/controllers/api/v8/core/courses_controller.rb
+++ app/controllers/api/v8/core/courses_controller.rb
@@ -30,6 +30,10 @@ module Api
 
         def show
           unauthorize_guest!
+          if params[:client] == 'netbeans_plugin' && params[:client_version] = '1.1.9'
+            authorization_skip!
+            return respond_with_error("You need to update your client. You can do that by selecting 'Help' -> 'Check for updates' and then following instructions.", 404, nil, obsolete_client: true)
+          end
           course = Course.find_by!(id: params[:id])
           authorize! :read, course
           data = { course: CourseInfo.new(current_user, view_context).course_data_core_api(course) }
--- app/controllers/api/v8/core/exercises/submissions_controller.rb
+++ app/controllers/api/v8/core/exercises/submissions_controller.rb
@@ -34,10 +34,6 @@ module Api
 
           def create
             unauthorize_guest!
-            if params[:client] == 'netbeans_plugin' && params[:client_version] = '1.1.9'
-              authorization_skip!
-              return respond_with_error("You need to update your client. You can do that by selecting 'Help' -> 'Check for updates' and then following instructions.", 404, nil, obsolete_client: true)
-            end
 
             @exercise = Exercise.find(params[:exercise_id])
             authorize! :read, @exercise
