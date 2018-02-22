94c1e2bf2cb2943a77d64d0319f6c570a7a27632
Henrik Nygren <nygrenh@gmail.com>
2018-02-22 16:51:30 +0200

    Add new organization request mailer

---
+++ app/mailers/new_organization_request_mailer.rb
@@ -0,0 +1,11 @@
+class NewOrganizationRequestMailer < ActionMailer::Base
+  def request_email(organization)
+    email = SiteSetting.value('administrative_email')
+    if email.empty?
+      Rails.logger.warn 'Not sending a notification on a new organization because administrative_email is not defined. '
+    end
+    @organization = organization
+    @creator = organization.creator
+    mail(to: email, subject: 'New TestMyCode Organization Request')
+  end
+end
