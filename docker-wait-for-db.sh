edd229e61eeaa881f5fb0b910626a7f2a9039fb4
Henrik Nygren <nygrenh@gmail.com>
2018-04-04 19:07:06 +0300

    Run v8 tests in travis

---
+++ docker-wait-for-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+
+cmd=$*
+
+echo "Waiting for postgres..."
+until psql -h db -p 5432 -U "postgres" -c '\l' 2> /dev/null; do
+  >&2 echo -n "."
+  sleep 1
+done
+echo ""
+if [ ! -z "$cmd" ]; then
+  echo Running "$cmd"
+  exec $cmd
+fi
