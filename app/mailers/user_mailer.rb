2a480e6567588f43c0bd116b2175bfc09f38a55f
Henrik Nygren <nygrenh@gmail.com>
2018-04-25 14:56:18 +0300

    Change email branding

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
     @user = user
     token = user.verification_tokens.email.create!
     @url = base_url + confirm_email_path(@user.id, token.token)
-    mail(from: SiteSetting.value('emails')['from'], to: user.email, subject: "Confirm your TestMyCode Account email address")
+    mail(from: SiteSetting.value('emails')['from'], to: user.email, subject: "Confirm your mooc.fi account email address")
   end
 
   def destroy_confirmation(user)
     @user = user
     token = user.verification_tokens.delete_user.create!
     @url = base_url + verify_destroying_user_path(@user.id, token.token)
-    mail(from: SiteSetting.value('emails')['from'], to: user.email, subject: "Confirm deleting your TestMyCode account")
+    mail(from: SiteSetting.value('emails')['from'], to: user.email, subject: "Confirm deleting your mooc.fi account")
   end
 
 
