64d8b31363d2dbb0bad9aa087c9e7fa367670429
Henrik Nygren <nygrenh@gmail.com>
2018-05-10 08:58:20 +0300

    Allow longer usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
 
   validates :login, presence: true,
                     uniqueness: { case_sensitive: false },
-                    length: { within: 2..20 }
+                    length: { within: 2..50 }
 
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false }
