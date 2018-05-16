2b21cbc02ab827c255fef12e9b2642a52d3d0cec
Henrik Nygren <nygrenh@gmail.com>
2018-05-16 12:32:25 +0300

    Fix typo

--- app/controllers/api/v8/users_controller.rb
+++ app/controllers/api/v8/users_controller.rb
@@ -72,7 +72,7 @@ module Api
         if params[:show_user_fields]
           user_field = {}
           UserField.all.select { |f| f.visible_to?(current_user) }.each do |field|
-            value_record = @user.field_value_record(field)
+            value_record = user.field_value_record(field)
             value = value_record.ruby_value
             user_field[field] = value
           end
