af45155793c575ddfba667efa4fa72e7f952e81c
Henrik Nygren <nygrenh@gmail.com>
2018-11-08 13:42:21 +0200

    Update ai redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -87,7 +87,7 @@ class UsersController < ApplicationController
   def confirm_email
     token = VerificationToken.email.find_by!(user_id: params[:user_id], token: params[:id])
     redirect_path = root_url
-    redirect_path = 'https://course.elementsofai.com/email-verification' if params[:origin] == 'elements_of_ai'
+    redirect_path = 'https://course.elementsofai.com/email-verification' if params[:origin].start_with?('elements_of_ai')
     User.find(params[:user_id]).update!(email_verified: true)
     redirect_to redirect_path, notice: 'Your email address has been verified!'
   end
