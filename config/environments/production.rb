c0bf22daa4bba4910305be017f8833b52ccbd7e1
Henrik Nygren <nygrenh@gmail.com>
2018-11-27 19:37:13 +0200

    Enable sending emails with SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,21 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Mail delivery method
-  config.action_mailer.delivery_method = :sendmail
+  if ENV['USE_SMTP']
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              ENV['SMTP_ADDRESS'],
+      port:                 ENV['SMTP_PORT'],
+      domain:               ENV['SMTP_DOMAIN'],
+      user_name:            ENV['SMTP_USERNAME'],
+      password:             ENV['SMTP_PASSWORD'],
+      authentication:       ENV['SMTP_AUTHENTICATION'],
+      enable_starttls_auto: true
+    }
+  else
+    config.action_mailer.delivery_method = :sendmail
+  end
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
