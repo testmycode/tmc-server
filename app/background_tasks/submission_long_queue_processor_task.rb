b5a6cf929ea863c8a9fded269f3584698eb2bcfd
Henrik Nygren <nygrenh@gmail.com>
2018-12-28 16:27:15 +0200

    Add reverse processor for long queues

---
+++ app/background_tasks/submission_long_queue_processor_task.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'submission_processor'
+
+class SubmissionLongQueueProcessorTask
+  def initialize
+    @processor = SubmissionProcessor.new
+  end
+
+  def run
+    queue = Submission.to_be_reprocessed.where(processing_priority: 0).order(:created_at).reverse_order
+    return if queue.length <= 10
+    Rails.logger.info "#{queue.length} high priority submissions in queue, trying to process some of them in reverse order..."
+    queue.limit(RemoteSandbox.total_capacity).each do |sub|
+      Rails.logger.info "Processing submission #{sub} in from the front of the queue since submission queue is so long..."
+      @processor.process_submission(sub)
+      Rails.logger.info "Processing submission #{sub} done"
+    end
+  end
+
+  def wait_delay
+    1
+  end
+end
