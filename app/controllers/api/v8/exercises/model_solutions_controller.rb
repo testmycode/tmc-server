11c6d17a11470be3335bed4c6ba848e7ecee143f
Antti Leinonen <antti.leinonen@helsinki.fi>
2019-01-04 15:16:21 +0200

    Don't use a model solution token again on same exercise

--- app/controllers/api/v8/exercises/model_solutions_controller.rb
+++ app/controllers/api/v8/exercises/model_solutions_controller.rb
@@ -15,7 +15,7 @@ module Api
           rescue CanCan::AccessDenied
             model_solution_token_used_on_this_exercise = ModelSolutionTokenUsed.where(user: current_user, course: course, exercise_name: exercise.name).count > 0
             grant_model_solution_token_every_nth_completed_exercise = course.grant_model_solution_token_every_nth_completed_exercise
-            if grant_model_solution_token_every_nth_completed_exercise && grant_model_solution_token_every_nth_completed_exercise > 0
+            if grant_model_solution_token_every_nth_completed_exercise && grant_model_solution_token_every_nth_completed_exercise > 0 && !model_solution_token_used_on_this_exercise
               completed_exercises_count = course.submissions.where(all_tests_passed: true, user: current_user).distinct.select(:exercise_name).count
               total_model_solution_tokens = completed_exercises_count / grant_model_solution_token_every_nth_completed_exercise
 
@@ -24,7 +24,7 @@ module Api
               if available_model_solution_tokens > 0
                 ModelSolutionTokenUsed.create!(user: current_user, course: course, exercise_name: exercise.name)
               else
-                raise CanCan::AccessDenied unless model_solution_token_used_on_this_exercise
+                raise CanCan::AccessDenied
               end
             else
               raise CanCan::AccessDenied unless model_solution_token_used_on_this_exercise
