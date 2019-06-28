46dd748d8548698fa593b0ddceeb29755c878c8a
Henrik Nygren <nygrenh@gmail.com>
2019-06-28 16:15:36 +0300

    Fix typo

--- app/controllers/api/v8/user_app_datum_controller.rb
+++ app/controllers/api/v8/user_app_datum_controller.rb
@@ -4,9 +4,10 @@ module Api
   module V8
     class UserAppDatumController < Api::V8::BaseController
 
+      skip_authorization_check
       def index
         only_admins!
-        skip_authorization_check
+
         render json: UserAppDatum.all.to_json
       end
     end
