d48017d624aab6dd6a1a457fbf123708005194cc
Henrik Nygren <nygrenh@gmail.com>
2019-11-12 11:40:43 +0200

    Add de translation to signup title

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,6 +11,7 @@ class UserMailer < ActionMailer::Base
     subject = 'Confirm your mooc.fi account email address'
     subject = 'Varmista mooc.fi tunnuksesi sähköpostiosoite' if language == "fi"
     subject = 'Bekräfta e-postadressen till ditt mooc.fi-konto' if language == "se"
+    subject = 'Bestätige deine E-Mail-Adresse, um mit dem Kurs zu beginnen' if language == "de"
     subject = "#{origin}: #{subject}" if origin
     if origin
       origin_name = origin.downcase.tr(' ', '_').gsub(/[\.\/]/, '')
