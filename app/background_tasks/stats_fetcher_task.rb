43de073d8bb06e069c64ea1a4900acde11e918d7
Henrik Nygren <nygrenh@gmail.com>
2020-01-10 15:45:50 +0200

    Fix task

--- app/background_tasks/stats_fetcher_task.rb
+++ app/background_tasks/stats_fetcher_task.rb
@@ -1,20 +1,18 @@
-
-
-class SandboxStatusFetcherTask
+class StatsFetcherTask
   def initialize
   end
 
   def run
     data = {
-      high_priority_submissions_count: Submission.to_be_reprocessed.where(processing_priority: 0).count
-      sandbox_queue_length: Submission.to_be_reprocessed.count
-      unprocessed_submissions_count: Submission.where(processed: false).count
-      submissions_count_minute: Submission.where(created_at: (Time.current - 1.minute)..Time.current).count
-      submissions_count_five_minutes: Submission.where(created_at: (Time.current - 5.minute)..Time.current).count
-      submissions_count_hour: Submission.where(created_at: (Time.current - 1.hour)..Time.current).count
-      submissions_count_today: Submission.where(created_at: Time.current.all_day).count
-      submissions_count_yesterday: Submission.where(created_at: Time.current.yesterday.all_day).count
-      submissions_count_week: Submission.where(created_at: Time.current.all_week).count
+      high_priority_submissions_count: Submission.to_be_reprocessed.where(processing_priority: 0).count,
+      sandbox_queue_length: Submission.to_be_reprocessed.count,
+      unprocessed_submissions_count: Submission.where(processed: false).count,
+      submissions_count_minute: Submission.where(created_at: (Time.current - 1.minute)..Time.current).count,
+      submissions_count_five_minutes: Submission.where(created_at: (Time.current - 5.minute)..Time.current).count,
+      submissions_count_hour: Submission.where(created_at: (Time.current - 1.hour)..Time.current).count,
+      submissions_count_today: Submission.where(created_at: Time.current.all_day).count,
+      submissions_count_yesterday: Submission.where(created_at: Time.current.yesterday.all_day).count,
+      submissions_count_week: Submission.where(created_at: Time.current.all_week).count,
     }
     Rails.cache.write("stats-cache", data.to_json, expires_in: 1.minute)
   end
