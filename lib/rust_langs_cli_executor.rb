cd14ea79bc42d1c660ea20f1d8ea44036432eb7e
Antti Leinonen <antti.leinonen@helsinki.fi>
2020-08-26 16:58:13 +0300

    Revert "Revert "Use rust for submission packaging when downloading as zip""
    This reverts commit 9be7d10c7348928e97f3759683daad0f86bc621a.

--- lib/rust_langs_cli_executor.rb
+++ lib/rust_langs_cli_executor.rb
@@ -5,15 +5,26 @@ require 'fileutils'
 # If you want to change the TMC Langs Rust version, see app/background_tasks/rust_langs_downloader_task.rb
 
 module RustLangsCliExecutor
-  def self.prepare_submission(clone_path, output_path, submission_path)
+  def self.prepare_submission(clone_path, output_path, submission_path, extra_params = {}, config = {})
     command = "vendor/tmc-langs-rust/current prepare-submission --clone-path #{clone_path} --output-path #{output_path} --submission-path #{submission_path}"
+
+    command = command + " --top-level-dir-name #{config[:toplevel_dir_name]}" if !!config[:toplevel_dir_name]
+
+    command = command + " --stub-zip-path #{config[:tests_from_stub]}" if !!config[:tests_from_stub]
+
+    command = command + " --output-format #{config[:format]}" if !!config[:format]
+
+    extra_params.each do |k, v|
+      command = command + " --tmc-param #{k}=#{v}" unless v.nil?
+    end
+
     command_output = `#{command}`
     Rails.logger.info command_output
 
     result = self.process_command_output(command_output)
 
     if result['result'] == 'error'
-      Rails.logger.info 'Preparing submission failed: ' + result.to_s
+      Rails.logger.info('Preparing submission failed: ' + result.to_s)
     end
 
     result
@@ -34,8 +45,8 @@ module RustLangsCliExecutor
       last_line = valid_lines.last
 
       if last_line['status'] == 'crashed'
-        Rails.logger.info 'TMC-langs-rust crashed'
-        Rails.logger.info last_line
+        Rails.logger.info('TMC-langs-rust crashed')
+        Rails.logger.info(last_line)
         raise 'TMC-langs-rust crashed: ' + last_line
       end
 
