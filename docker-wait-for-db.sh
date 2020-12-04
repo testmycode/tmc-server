59a6f650cb86c1516d4c5dc95449b6b765331164
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2020-12-04 09:54:42 +0200

    Fix psql

--- docker-wait-for-db.sh
+++ docker-wait-for-db.sh
@@ -3,12 +3,12 @@ set -eu
 
 cmd=$*
 
-echo "Waiting for postgres..."
-until psql -h db -p 5432 -U "postgres" -c '\l' 2> /dev/null; do
-  >&2 echo -n "."
-  sleep 1
-done
-echo ""
+# echo "Waiting for postgres..."
+# until psql -h "$DB_HOST" -p 5432 -U "postgres" -c '\l' 2> /dev/null; do
+#   >&2 echo -n "."
+#   sleep 1
+# done
+# echo ""
 if [ ! -z "$cmd" ]; then
   echo Running "$cmd"
   exec $cmd
