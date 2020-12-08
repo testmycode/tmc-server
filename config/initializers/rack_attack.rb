7bac097d75e7a990bc3e997939846457070da8c2
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2020-12-08 14:24:10 +0200

    Rubocop

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class Rack::Attack
-    # Return 500 Internal Server Error for throttles
-    self.throttled_response = lambda do |env|
-        [ 503, # status
-          {}, # headers
-          ["Service Unavailable\n"] # body
-        ]
-    end
+  # Return 500 Internal Server Error for throttles
+  self.throttled_response = lambda do |env|
+    [ 503, # status
+      {}, # headers
+      ["Service Unavailable\n"] # body
+    ]
+  end
 
   # Limit the number of logins to 20 attempts in a minute
   throttle('login attempts', limit: 20, period: 1.minutes) do |req|
