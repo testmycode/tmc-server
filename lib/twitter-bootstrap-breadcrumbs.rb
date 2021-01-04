2873c721b8af48563dabc73bb82dad371642020f
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-01-04 18:15:26 +0200

    Fix regexp that rubocop autocorrected

--- lib/twitter-bootstrap-breadcrumbs.rb
+++ lib/twitter-bootstrap-breadcrumbs.rb
@@ -19,7 +19,7 @@ module BreadCrumbs
     def add_breadcrumb(name, url = '', options = {})
       @breadcrumbs ||= []
       name = translate_breadcrumb(name, self.class.name) if name.is_a?(Symbol)
-      url = eval(url.to_s) if /_path|_url|@/.match?(url)
+      url = eval(url.to_s) if url =~ /_path|_url|@/ # rubocop:disable Performance/RegexpMatch
       @breadcrumbs << { name: name, url: url, options: options }
     end
 
