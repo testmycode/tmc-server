f496c7168c89edb3dcf49de07fe390ee51352d05
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-02-10 13:51:12 +0200

    js_errors false

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,7 +31,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # In console 1: Xvfb :99
 # In console 2: env DISPLAY=:99 rvmsudo rake spec
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, timeout: 60)
+  Capybara::Poltergeist::Driver.new(app, { timeout: 60, js_errors: false })
 end
 
 Capybara.default_driver = :poltergeist
