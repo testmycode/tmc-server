e5439465371cc92c3b546646fd6dc4a36f19afb6
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-02-11 15:06:23 +0200

    Remove paperclip attachment

--- db/migrate/20150701054936_add_attachment_logo_to_organizations.rb
+++ db/migrate/20150701054936_add_attachment_logo_to_organizations.rb
@@ -1,11 +1,11 @@
 class AddAttachmentLogoToOrganizations < ActiveRecord::Migration[4.2]
   def self.up
-    change_table :organizations do |t|
-      t.attachment :logo
-    end
+    # change_table :organizations do |t|
+    #   t.attachment :logo
+    # end
   end
 
-  def self.down
-    remove_attachment :organizations, :logo
-  end
+  # def self.down
+  #   remove_attachment :organizations, :logo
+  # end
 end
