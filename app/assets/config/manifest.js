0afbea1a32cf548b942ec2888b393d11ff3969eb
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-02-12 09:57:45 +0200

    Upgrade to sprocket 4

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,8 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../javascripts/setup .js
+//= link_directory ../stylesheets .css
+//= link_directory ../../../vendor/assets/images
+//= link_directory ../../../vendor/assets/javascripts .js
+//= link_directory ../../../vendor/assets/javascripts/ace .js
+//= link_directory ../../../vendor/assets/stylesheets .css
