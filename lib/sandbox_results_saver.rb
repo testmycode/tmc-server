f8a1bdd87c11dd704ab164f9d3178abe2c76b880
Antti Leinonen <antti.leinonen@helsinki.fi>
2021-06-02 17:20:39 +0300

    Update sandbox result saver to new langs response format

--- lib/sandbox_results_saver.rb
+++ lib/sandbox_results_saver.rb
@@ -30,8 +30,10 @@ module SandboxResultsSaver
         submission.pretest_error =
           case results['exit_code']
           when '110'
-            tmc_langs_response['status'] + "\n" +
-            tmc_langs_response['logs'].map do |k, v| "#{k}: #{v}" end.join("\n")
+            error_msg = tmc_langs_response['status']
+            error_msg = error_msg + "\n" + tmc_langs_response['logs'].map do |k, v| "#{k}: #{v}" end.join("\n") if tmc_langs_response['logs']
+            error_msg = error_msg + "\n" + tmc_langs_response['data']['output-data']['trace'].map do |k, v| "#{k}: #{v}" end.join("\n") if tmc_langs_response.dig('data', 'output-data', 'trace')
+            error_msg
           when '137'
             'Program was forcibly terminated, most likely due to using too much time or memory.'
           when nil
