3986c71a0b7d135c9e22c633cd272477f013d192
Mikko Pyykkö <mipyykko@gmail.com>
2021-09-03 15:34:17 +0300

    accept moocfi tokens

--- app/controllers/api/v8/base_controller.rb
+++ app/controllers/api/v8/base_controller.rb
@@ -35,9 +35,22 @@ module Api
 
       private
         def authenticate_user!
+          puts "current_user", @current_user
+          puts "token", bearer_token
+
           return @current_user if @current_user
+
           if doorkeeper_token
             @current_user ||= User.find_by(id: doorkeeper_token.resource_owner_id)
+            raise 'Invalid token' unless @current_user
+          else
+            moocfi_user = validate_moocfi_user
+            puts "moocfi_user", moocfi_user
+            raise 'Invalid token' unless moocfi_user
+
+            @current_user ||= User.find_by(id: moocfi_user['upstream_id']) if moocfi_user['upstream_id'] > 0 
+            @current_user ||= create_user_from_moocfi(moocfi_user) if not moocfi_user['upstream_id'] or moocfi_user['upstream_id'] < 0
+            puts "current_user after find or create", @current_user
             raise 'Invalid token' unless @current_user
           end
           @current_user ||= user_from_session || Guest.new
@@ -124,6 +137,73 @@ module Api
               nil # without version check
             end
           end
+        end
+
+        def bearer_token
+          pattern = /^Bearer /
+          authorization = request.authorization
+          authorization.gsub(pattern, '') if authorization && authorization.match(pattern)
+        end
+
+        def validate_moocfi_user
+          uri = URI('http://localhost:4000/auth/validate')
+          req = Net::HTTP::Get.new(uri)
+          req["Authorization"] = request.authorization
+
+          mooc_response = Net::HTTP.start(uri.hostname, uri.port) {|http|
+            res = http.request(req)
+            JSON.parse(res.body)
+          }
+          user = mooc_response["user"]
+          puts user
+          user 
+          # @current_user ||= user
+          # @current_user ||= User.new({
+          #   id: user.id,
+          #   login: user.username,
+          #   email: user.email
+          # })
+
+          # .find_by(id: mooc_response["user"]["upstream_id"])
+        end
+
+        def create_user_from_moocfi(moocfi_user)
+          puts "creating new user from", moocfi_user
+          user = User.new
+          user.login = SecureRandom.uuid
+          user.email = moocfi_user['email']
+          user.password = SecureRandom.base64(12)
+          user.administrator = moocfi_user['administrator'] || false
+
+          user.save!
+
+          first_name = UserFieldValue.new(field_name: 'first_name', user_id: user.id, value: moocfi_user['first_name'])
+          last_name = UserFieldValue.new(field_name: 'last_name', user_id: user.id, value: moocfi_user['last_name'])
+          # first_name = user.user_field_values.new(field_name: 'first_name', value: moocfi_user['first_name'])
+          # last_name = user.user_field_values.new(field_name: 'last_name', value: moocfi_user['last_name'])
+
+          first_name.save!
+          last_name.save!
+
+          puts "new user?", user.to_json
+
+          update_moocfi_user(user)
+
+          user
+        end 
+        
+        def update_moocfi_user(user)
+          uri = URI('http://localhost:4000/api/user/update-from-tmc')
+          req = Net::HTTP::Post.new(uri, initheader = { 'Content-Type' => 'application/json' })
+          req["Authorization"] = request.authorization
+          req.body = { upstream_id: user['id'] }.to_json
+
+          mooc_response = Net::HTTP.start(uri.hostname, uri.port) {|http|
+            http.request(req)
+          }
+          puts mooc_response.code
+          raise "Error updating MOOC.fi user" if mooc_response.code.to_i >= 400
+
         end
     end
   end
