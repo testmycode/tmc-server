a3ba9038262551317c7a213190662aa60032dddf
Henrik Nygren <nygrenh@gmail.com>
2022-03-24 12:32:59 +0200

    Add KafkaBatchUpdatePoints.send_points_again_for_user_and_course

--- app/models/kafka_batch_update_points.rb
+++ app/models/kafka_batch_update_points.rb
@@ -2,4 +2,13 @@
 
 class KafkaBatchUpdatePoints < ApplicationRecord
   belongs_to :course
+
+  def self.send_points_again_for_user_and_course(course_id, user_id)
+    transaction do
+      create!(course_id: course_id, user_id: user_id, realtime: false, task_type: "user_progress")
+      Exercise.where(course_id: 600).each do |exercise|
+        create!(course_id: course_id, user_id: user_id, exercise_id: exercise.id realtime: false, task_type: "user_points")
+      end
+    end
+  end
 end
